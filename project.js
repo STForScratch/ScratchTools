5b49d4ecebab903fb17858f4a92074dc2e4c2b1f
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-02-28 17:22:19 -0800

    Fixed bugs for replacing profile project links

--- project.js
+++ project.js
@@ -1,12 +1,67 @@
-if (window.location.href.includes('editor')) {
-} else {
+setTimeout(() => { replacealllinks() }, 2000);
+setTimeout(() => { addOnClick() }, 2000);
+about()
 str = window.location.href
-var apple = str.split('https://scratch.mit.edu/projects/')[1];
-var apple2 = apple.split('/')[0];
-getapi2(`https://api.scratch.mit.edu/projects/${apple2}/`)
+document.querySelector('#profile-avatar > div > a > img').style.borderRadius = "10%";
+document.querySelector('#profile-avatar > div > a > img').style.border = "0px";
+var elementExists = document.querySelector('#featured-project > img')
+if (elementExists !== null) {
+document.querySelector('#featured-project > img').style.borderRadius = "20px";
 }
+const help = ["rgantzos", "lisa_wolfgang", "--Explosion--", "GarboMuffin", "Xancan", "-Jensen-", "-Intensify-", "-OutroCoder-"];
+if (help.includes(document.querySelector('#profile-data > div.box-head > div > h2').textContent)) {
+document.querySelector('#profile-data > div.box-head > div > p > span.group').textContent = `${document.querySelector('#profile-data > div.box-head > div > p > span.group').textContent} | ScratchTools`
+document.querySelector('#profile-data > div.box-head > div > h2').textContent = `*${document.querySelector('#profile-data > div.box-head > div > h2').textContent}`
+setTimeout(() => { replacealllinks() }, 2000);
+setTimeout(() => { addOnClick() }, 2000);
+}
+// replace links in comments
+function replacealllinks() {
+addOnClick()
+const highlightedItems = document.querySelectorAll("a");
 
-async function getapi2(url) {
+highlightedItems.forEach(function(item) {
+  if (item.href.includes('https://scratch.mit.edu/projects/')) {
+if (item.parentNode.className === 'content') {
+replacelinks(item)
+}
+  }
+});
+
+async function replacelinks(item) {
+    
+    // Storing response
+    const response = await fetch(`https://api.${item.href.replace('https://', '')}`);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+if(data.hasOwnProperty('title')){
+    var stuff = data["title"]
+item.textContent = stuff
+}
+    }
+  }
+  function addOnClick() {
+    if (document.querySelector('#profile-data > div.box-head > div.header-text > h2').textContent === document.querySelector('#topnav > div > div > ul.account-nav.logged-in > li.logged-in-user.dropdown > span').textContent) {
+    var thebutton = document.querySelector('#comments > div:nth-child(3) > ul > div')
+    } else {
+      var thebutton = document.querySelector('#comments > div:nth-child(2) > ul > div')
+    }
+    thebutton.onclick = function() {
+      setTimeout(() => { replacealllinks() }, 2000);
+    }
+  }
+    // done replacing
+
+var apple = str.split('https://scratch.mit.edu/users/')[1];
+  var apple2 = apple.split('/')[0];
+  getapi3(`https://api.scratch.mit.edu/users/${apple2}/`, apple2);
+  var elementExists = document.querySelector("div.template-feature-off.comments-off")
+if (elementExists !== null) {
+  document.querySelector("div.template-feature-off.comments-off").textContent = 'This user has chosen to turn off their profile comments.';
+}
+async function getapi3(url, user) {
     
     // Storing response
     const response = await fetch(url);
@@ -14,101 +69,36 @@ async function getapi2(url) {
     // Storing data in form of JSON
     var data = await response.json();
     console.log(data);
-    var url = window.location.href;
-    var stuff = data["code"]
-    if (1 === 2) {
-        window.location.href = `https://turbowarp.org/${apple2}`
-    } else {
-        setTimeout(() => { proposebutton() }, 2000);
-    }
+    var country = data["profile"]["country"]
+    console.log(country)
+    getapi2(`https://api.scratch.mit.edu/users/${user}/messages/count/`, country);
 }
+// Calling that async function
 
-async function getapistuff(url) {
+async function getapi2(url, a) {
     
     // Storing response
-    const response = await fetch(`https://api.scratch.mit.edu/projects/${url}/`);
+    const response = await fetch(url);
     
     // Storing data in form of JSON
     var data = await response.json();
     console.log(data);
-    var url = window.location.href;
-    var stuff = data["author"]["username"]
-    if (stuff === document.querySelector('#navigation > div > ul > li.link.right.account-nav > div > a > span').textContent) {
-        // when done set to abc()
-    } else {
-        xyz()
-    }
+    var stuff = data["count"]
+    document.querySelector(".location").textContent = `${a} | ${stuff} Messages`;
 }
+// Calling that async function
 
-async function getapi3(url, abc) {
-    
-    // Storing response
-    const response = await fetch('https://scratchdb.lefty.one/v3/user/info/' +url);
-    
-    // Storing data in form of JSON
-    var data = await response.json();
-    console.log(data);
-    if (JSON.stringify(data) !== '{"error":"UserNotFoundError"}') {
-    var stuff = data["statistics"]["followers"]
-    document.querySelector('#view > div > div.inner > div.flex-row.preview-row.force-row > div.flex-row.project-header > div > a').textContent = `${abc} - ${stuff} Followers (Credit to ScratchDB by DatOneLefty)`
-}
-}
-
-function propose() {
-    alert('Hello')
-}
-
-function proposebutton() {
-getapistuff(apple2)
-}
-
-function abc() {
-    var elem = document.createElement('button');
-    elem.textContent = 'Set Featured';
-    elem.className = 'button action-button feature-button'
-    elem.onclick = function() {
-        stuff()
-        alert('The project has been set as featured!')
-      }
-    document.querySelector('#view > div > div.inner > div:nth-child(3) > div.flex-row.subactions > div.flex-row.action-buttons').appendChild(elem);
-    }
-
-    function xyz() {
-getapi3(document.querySelector('#view > div > div.inner > div.flex-row.preview-row.force-row > div.flex-row.project-header > div > a').textContent, document.querySelector('#view > div > div.inner > div.flex-row.preview-row.force-row > div.flex-row.project-header > div > a').textContent)
-
-var elem = document.createElement('button');
-elem.textContent = 'Propose';
-elem.className = 'button action-button propose-button'
-var el = document.createElement('a');
-el.href = 'https://scratch.mit.edu/studios/28715018/comments/'
-document.querySelector('#view > div > div.inner > div:nth-child(3) > div.flex-row.subactions > div.flex-row.action-buttons').appendChild(el);
-document.querySelector('#view > div > div.inner > div:nth-child(3) > div.flex-row.subactions > div.flex-row.action-buttons > a').appendChild(elem);
-}
-
-function stuff() {
-    str = window.location.href
-    var apple = str.split('https://scratch.mit.edu/projects/')[1];
-  var stuff = apple.split('/')[0];
-    stuff2 = document.querySelector('#navigation > div > ul > li.link.right.account-nav > div > a > span').textContent
-    fetch("https://scratch.mit.edu/site-api/users/all/" +stuff2+ "/", {
-    "headers": {
-      "accept": "application/json, text/javascript, */*; q=0.01",
-      "accept-language": "en-US,en;q=0.9",
-      "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-      "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"97\", \"Chromium\";v=\"97\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"macOS\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "x-csrftoken": "rJrBdCSe4VX1eEwb9VE4wCbc6V85KXUd",
-      "x-requested-with": "XMLHttpRequest"
-    },
-    "referrer": "https://scratch.mit.edu/users/" +stuff2+ "/",
-    "referrerPolicy": "strict-origin-when-cross-origin",
-    "body": "{\"featured_project\":" +stuff+ ",\"featured_project_label\":\"\"}",
-    "method": "PUT",
-    "mode": "cors",
-    "credentials": "include"
-  });
+function about() {
+  if (getCookie('hideabout?') === 'yes') {
+    document.querySelector('#topnav > div > div > ul.site-nav > li.last').remove()
   }
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+}
+
+function feature() {
+
+}
