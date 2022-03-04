e63cb83e14016878e02961b2145c4473627a614d
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-03-03 20:34:28 -0800

    Added code for search all projects

---
+++ search.js
@@ -0,0 +1,12 @@
+if (window.location.href.includes('%23')) {
+} else {
+var btn = document.createElement('button')
+btn.className = 'button showAll'
+btn.textContent = 'Show All'
+btn.onclick = function() {
+    str = window.location.href
+var apple = str.split('https://scratch.mit.edu/search/projects?q=')[1];
+    window.location.href = `https://scratch.mit.edu/search/projects?q=%23${apple.replaceAll('%20', "%20%23")}`
+}
+document.querySelector('#view > div > div > div.sort-controls').appendChild(btn)
+}
