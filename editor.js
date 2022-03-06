1ed85f68d738db31ad1b13816f91522ccefd30db
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-03-06 15:22:46 -0800

    Fixed bugs with loading and added feature of hiding sprite watermark

--- editor.js
+++ editor.js
@@ -1,3 +1,19 @@
+function checkFlag() {
+    if(document.querySelector('#app > div > div.gui_menu-bar-position_3U1T0.menu-bar_menu-bar_JcuHF.box_box_2jjDp > div.menu-bar_main-menu_3wjWH') === null) {
+       window.setTimeout(checkFlag, 100); /* this checks the flag every 100 milliseconds*/
+    } else {
+      document.querySelector('#react-tabs-1 > div.gui_watermark_3vBYb.box_box_2jjDp > img').remove()
+    }
+}
+checkFlag();
+checkFlag2();
+function checkFlag2() {
+    if(document.querySelector('#app > div > div.gui_menu-bar-position_3U1T0.menu-bar_menu-bar_JcuHF.box_box_2jjDp > div.menu-bar_main-menu_3wjWH > div:nth-child(7)') === null) {
+        window.setTimeout(checkFlag2, 100)
+    } else {
+        editor()
+    }
+}
 function editor() {
 var element = document.createElement('a')
 var str = window.location.href
@@ -11,5 +27,3 @@ document.querySelector('#app > div > div.gui_menu-bar-position_3U1T0.menu-bar_me
 document.querySelector('#app > div > div.gui_menu-bar-position_3U1T0.menu-bar_menu-bar_JcuHF.box_box_2jjDp > a > div > span > div > span').textContent = 'Open in Turbowarp'
 document.querySelector('#app > div > div.gui_menu-bar-position_3U1T0.menu-bar_menu-bar_JcuHF.box_box_2jjDp > a > div > span > img').src = 'https://dashboard.snapcraft.io/site_media/appmedia/2021/02/512x512_Q3PveGU.png'
 }
-console.log('')
-setTimeout(() => { editor() }, 5000);
