208eacb7ca7c49547796a4d2dc22f09d6e9f4bbd
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-03-11 09:04:32 -0800

    Fixed statistics bug

--- statistics.js
+++ statistics.js
@@ -5,6 +5,6 @@ document.querySelector('#tabs').appendChild(clone);
 document.querySelector('#tabs > li:nth-child(6) > a').textContent = 'Statistics'
 document.querySelector('#tabs > li:nth-child(6)').className = 'last'
 document.querySelector('#tabs > li:nth-child(6)').dataTab = 'stats'
-document.querySelector('#tabs > li:nth-child(6) > a').href = `https://scratchstats.com/${document.querySelector('#topnav > div > div > ul.account-nav.logged-in > li.logged-in-user.dropdown > span').textContent}`
+document.querySelector('#tabs > li:nth-child(6) > a').href = `https://scratchstats.com/${document.querySelector('#topnav > div > div > ul.account-nav.logged-in > li.logged-in-user.dropdown > div > ul > li:nth-child(1) > a').href.split('/users/')[1]}`
 }
 setTimeout(() => { stuff() }, 2000);
