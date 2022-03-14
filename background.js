1a3bfab43a571aecc725ce2cdb49acefef9ab5ef
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-03-14 07:24:49 -0700

    Changed to ScratchTools website

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => { // this event is triggered when the extension is installed or reloaded
-    chrome.tabs.create({ url: 'https://rgantzosonscratch.github.io/ScratchTools-Site/v1.6' }); // this is to open a webpage
+    chrome.tabs.create({ url: 'https://tools.scratchstatus.org/' }); // this is to open a webpage
 });
