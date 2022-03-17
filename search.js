7c77dc209e9911ab60ff411926f821704e802825
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-03-16 21:07:22 -0700

    Update search.js

--- search.js
+++ search.js
@@ -1,5 +1,13 @@
+// get cookie
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+  // get cookie
 if (window.location.href.includes('%23')) {
 } else {
+    if (getCookie('ST Features').includes('nfe')) {
 var btn = document.createElement('button')
 btn.className = 'button showAll'
 btn.textContent = 'Show All'
@@ -9,4 +17,5 @@ var apple = str.split('https://scratch.mit.edu/search/projects?q=')[1];
     window.location.href = `https://scratch.mit.edu/search/projects?q=%23${apple.replaceAll('%20', "%20%23")}`
 }
 document.querySelector('#view > div > div > div.sort-controls').appendChild(btn)
+}
 }
