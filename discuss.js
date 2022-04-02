538902b000bebf09d3b0c31fd19a72c91f386ca2
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-04-02 10:35:43 -0700

    Added draft features

--- discuss.js
+++ discuss.js
@@ -1,3 +1,8 @@
+if (getCookie('forum') === undefined) {
+  document.cookie = `forum=${text2Binary('No draft saved. Start writing and you can save a draft if necessary.')}; expires=Thu, 18 Dec 9999 12:00:00 UTC; path=/`;
+}
+
+
 // get cookie
 function getCookie(name) {
   const value = `; ${document.cookie}`;
@@ -6,18 +11,24 @@ function getCookie(name) {
 }
 // get cookie
 function replacealllinks() {
-  if (getCookie('ST Features').includes('forum-asterisk')) {
 const highlightedItems2 = document.querySelectorAll("a");
 
 highlightedItems2.forEach(function(item) {
   if (item.href.includes('https://scratch.mit.edu/users/')) {
 if (item.className === 'black username') {
+  if (getCookie('ST Features').includes('ocular-link')) {
+  var test = item.parentNode.cloneNode(true)
+test.firstChild.textContent = 'Open in Ocular'
+test.firstChild.href = `https://ocular.jeffalo.net/users/${item.textContent}/`
+item.parentNode.parentNode.appendChild(test)
+  }
     console.log('hi')
+    if (getCookie('ST Features').includes('forum-asterisk')) {
 replacelinks(item)
+    }
 }
   }
 })
-                         }
     async function replacelinks(item) {
     
     // Storing response
@@ -34,3 +45,49 @@ item.textContent = `${item.textContent}*`
 }
 }
 replacealllinks()
+
+if (getCookie('ST Features').includes('forum-draft')) {
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift()
+}
+
+
+checkforspace()
+function checkforspace() {
+    if (document.querySelector('#id_body') !== null) {
+        setCodeForText()
+    } else {
+        window.setTimeout(checkforspace(), 200)
+    }
+}
+
+function setCodeForText() {
+    var thebutton = document.createElement('button')
+    thebutton.textContent = 'Open Draft'
+    thebutton.className = 'grey button'
+    var thebutton2 = document.createElement('button')
+    thebutton2.textContent = 'Save Draft'
+    thebutton2.className = 'grey button'
+    thebutton.onclick = function() {
+      document.querySelector('#id_body').value = binaryToText(getCookie('forum'))
+    }
+    document.querySelector('#reply > div.box-content').appendChild(thebutton)
+    thebutton2.onclick = function() {
+      document.cookie = `forum=${text2Binary(document.querySelector('#id_body').value)}; expires=Thu, 18 Dec 9999 12:00:00 UTC; path=/`;
+    }
+    document.querySelector('#reply > div.box-content').appendChild(thebutton2)
+  }
+}
+function text2Binary(string) {
+  return string.split('').map(function (char) {
+      return char.charCodeAt(0).toString(2);
+  }).join(' ');
+}
+
+function binaryToText(binary) {
+binary = binary.split(' ');
+return binary.map(elem => String.fromCharCode(parseInt(elem, 2))).join("");
+}
