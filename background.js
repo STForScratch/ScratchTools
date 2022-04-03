78ac4a295a2191801a70fee0f0092610de009a3e
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-04-03 13:20:45 -0700

    Delete background.js

--- background.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => { // this event is triggered when the extension is installed or reloaded
-    chrome.tabs.create({ url: 'https://tools.scratchstatus.org/' }); // this is to open a webpage
-});
