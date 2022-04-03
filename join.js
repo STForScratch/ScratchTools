009575a9e030f3bdfe0635a0df0897c73f099ddf
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-04-03 13:19:23 -0700

    Delete join.js

--- join.js
+++
@@ -1,4 +0,0 @@
-var el = document.createElement('a')
-el.href = 'https://scratch.mit.edu/'
-el.textContent = 'Already have an account?'
-document.querySelector('body > div.ReactModalPortal > div > div > div > form > div > div:nth-child(1) > div > div:nth-child(3) > div.join-flow-password-section').appendChild(el)
