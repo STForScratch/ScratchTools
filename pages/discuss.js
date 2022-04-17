77e35545e68b6240c878424121309b83686f1fed
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-04-17 15:01:03 -0700

    Added new forums page feature and Zedexi page

--- pages/discuss.js
+++ pages/discuss.js
@@ -93,3 +93,47 @@ binary = binary.split(' ');
 return binary.map(elem => String.fromCharCode(parseInt(elem, 2))).join("");
 }
 }
+
+if (getCookie('ST Features').includes('new-forums-page')) {
+if (window.location.href.includes('https://scratch.mit.edu/discuss/m/topic/')) {
+    window.location.href = window.location.href.replace('https://scratch.mit.edu/discuss/m/topic/', 'https://scratch.mit.edu/discuss/topic/')
+}
+}
+
+var idk = []
+document.querySelectorAll('div').forEach(function(item) {
+    if (item.textContent.includes('save code') || item.textContent.includes('level code')) {
+        idk.push('found')
+    }
+})
+if (idk.includes('found')) {
+    var l1 = document.createElement('link')
+l1.rel = 'preconnect'
+l1.href = 'https://fonts.googleapis.com'
+document.head.appendChild(l1)
+var l2 = document.createElement('link')
+l2.rel = 'preconnect'
+l2.href = 'https://fonts.gstatic.com'
+document.head.appendChild(l2)
+var l3 = document.createElement('link')
+l3.href = 'https://fonts.googleapis.com/css2?family=DM+Sans&display=swap'
+l3.rel = 'stylesheet'
+document.head.appendChild(l3)
+    var thingy = document.querySelector('div.confirm-email.banner').cloneNode(true)
+    thingy.querySelector('span').textContent = 'Save your game codes with Zedexi instead of posting it in a game code topic to help Scratchers in need. Find out how to help '
+    document.querySelector('div#pagewrapper').prepend(thingy)
+    document.querySelector('div#pagewrapper').prepend(document.querySelector('div#topnav'))
+    thingy.style.display = ''
+    thingy.dataset.cue = ''
+    var thingy2 = document.createElement('a')
+    thingy2.textContent = 'here.'
+    thingy2.href = 'https://scratch.mit.edu/discuss/topic/596390/?page=1#post-6209353'
+    thingy2.target = '_blank'
+    thingy2.rel = 'noopener noreferrer'
+    thingy.querySelector('span').appendChild(thingy2)
+    thingy.style.backgroundColor = '#fcac38'
+    thingy.style.fontFamily = 'DM Sans'
+    thingy.querySelector('div.close').onclick = function() {
+        thingy.style.display = 'none'
+    }
+}
