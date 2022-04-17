cd3bf995cdc617c3cb6dc773b195a9005b4bbd56
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-04-16 19:26:54 -0700

    Added alert system

---
+++ extras/popup.js
@@ -0,0 +1,39 @@
+async function doStuff() {
+    const response = await fetch('https://tools.scratchstatus.org/warning/')
+    const data = await response.json()
+    if (data['title'] !== ' ') {
+        console.log(data['color'])
+        var div = document.createElement('div')
+        var title = data['title']
+        var title2 = document.createElement('h3')
+        title2.style.color = color
+        title2.textContent = title
+        var description = data['description']
+        var description2 = document.createElement('p')
+        description2.textContent = description
+        var color = data['color']
+        div.style.padding = '10px'
+        div.style.margin = '5px'
+        div.style.borderRadius = '10px'
+        div.style.border = `2px solid ${color}`
+        div.appendChild(title2)
+        div.appendChild(description2)
+        if (data['button'] !== ' ') {
+            var button = document.createElement('button')
+            button.addEventListener("click", openIndex);
+            button.style.border = '0px'
+            button.style.padding = '7px'
+            button.style.borderRadius = '5px'
+            button.style.backgroundColor = '#ff9f00'
+            button.style.color = 'white'
+
+function openIndex() {
+chrome.tabs.create({active: true, url: data['url']});
+}
+button.textContent = data['button']
+div.appendChild(button)
+        }
+        document.body.prepend(div)
+    }
+}
+doStuff()
