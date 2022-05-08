42f77073ffb8707bfa531c18d1659152fd5837ae
Electro-Corp <chinmaytanmay@outlook.com>
2022-05-08 08:00:59 -0600

    Added exact time person joined.
    Should work...

--- features/advanced-search.js
+++ features/advanced-search.js
@@ -15,9 +15,12 @@ async function checkfor(user) {
     var data = await response.json();
     if(data.hasOwnProperty('profile')){
     var logo = data['profile']['images']['90x90'];
+    var time = data['history']['joined'];
+    
 var userbox = document.createElement('div')
 var useravatar = document.createElement('img')
 var username = document.createElement('a')
+var timebox = document.createElement('div');
 username.textContent = `${user} is a user.`
 username.href = `https://scratch.mit.edu/users/${user}/`
 useravatar.src = logo
@@ -25,6 +28,13 @@ useravatar.style.height = '60px'
 useravatar.style.borderRadius = '4px'
 useravatar.style.padding = '10px'
 useravatar.style.float = 'left'
+// Time
+timebox.textContent = `Exact time joined: ${time}`
+timebox.style.height = '10px'
+timebox.style.borderRadius = '1px'
+timebox.style.padding = '1px'
+timebox.style.float = 'left'
+//
 username.style.float = 'right'
 userbox.appendChild(useravatar)
 username.style.padding = '30px'
@@ -34,6 +44,7 @@ userbox.style.margin = 'auto'
 userbox.style.height = '80px'
 userbox.style.width = '50vw'
 userbox.style.borderRadius = '10px'
+  
 document.querySelector('#projectBox').prepend(userbox)
 }
     }
