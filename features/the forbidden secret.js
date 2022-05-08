7403951b68bbc8b93b3d23c084b28ba1c14938aa
Penguin55 <93560458+SecretAgent55@users.noreply.github.com>
2022-05-08 17:37:21 -0400

    Create the forbidden secret.js

---
+++ features/the forbidden secret.js
@@ -0,0 +1,3 @@
+if (getCookie('ST Features').includes('secret')) {
+(function(){['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){document.body.style[prefix + 'transform'] = 'rotate(180deg)';});}());
+}
