1f733c4768422b66800d8624d444941c267cac5f
Penguin55 <93560458+SecretAgent55@users.noreply.github.com>
2022-05-09 19:21:52 -0400

    Update meow.js

--- features/meow.js
+++ features/meow.js
@@ -2,5 +2,17 @@ if (getCookie('ST Features').includes('meow')) {
 var elements = document.querySelector('#footer > div > ul.clearfix.footer-col > li:nth-child(3) > ul > li:nth-child(4)').cloneNode(true)
           var elementsappended = document.querySelector('#footer > div > ul.clearfix.footer-col > li:nth-child(3) > ul').parentNode.appendChild(elements)
           elementsappended.firstChild.textContent = 'meow'
+          elementsappended.firstChild.href = 'javascript:if (document.querySelector('#footer > div > ul.clearfix.footer-col > li:nth-child(3) > ul > li:nth-child(4)') !== null) {
+          var elements = document.querySelector('#footer > div > ul.clearfix.footer-col > li:nth-child(3) > ul > li:nth-child(4)').cloneNode(true)
+          var elementsappended = document.querySelector('#footer > div > ul.clearfix.footer-col > li:nth-child(3) > ul').parentNode.appendChild(elements)
+          elementsappended.firstChild.textContent = 'ScratchTools Settings'
           elementsappended.firstChild.href = 'https://genrandom.com/cats/'
+        }
+    
+        if (document.querySelector('#footer > div > div > dl:nth-child(3) > dd:nth-child(5)') !== null) {
+          var elements = document.querySelector('#footer > div > div > dl:nth-child(3) > dd:nth-child(5)').cloneNode(true)
+          var elementsappended = document.querySelector('#footer > div > div > dl:nth-child(3) > dd:nth-child(5)').parentNode.appendChild(elements)
+          elementsappended.firstChild.firstChild.textContent = 'Cat'
+          elementsappended.firstChild.href = 'https://genrandom.com/cats/'
+        }'
 }
