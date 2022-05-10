cc399e3fea742eea82976d148476c15039282430
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-05-10 06:51:29 -0700

    Update unbold-forum-topics.js

--- features/unbold-forum-topics.js
+++ features/unbold-forum-topics.js
@@ -6,4 +6,12 @@ document.querySelectorAll('*').forEach(function(el) {
         window.setTimeout(unbold, 100)
 }
 }
+if (getCookie('ST Features').includes('unbold-forum-topics')) {
 unbold()
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
