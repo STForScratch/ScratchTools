4ea8c13004494eb733f0f2681fc856d336b978f1
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-05-12 07:04:09 -0700

    Update compact-navbar.js

--- features/compact-navbar.js
+++ features/compact-navbar.js
@@ -1,3 +1,9 @@
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+
 if (getCookie('ST Features').includes('compact-navbar')) {
         if (document.querySelector('#footer > div > ul.clearfix.footer-col > li:nth-child(3) > ul > li:nth-child(4)') !== null) {
           document.querySelector('#topnav > div > div > ul.account-nav.logged-in > li.logged-in-user.dropdown > span').innerHTML = document.querySelector('#topnav > div > div > ul.account-nav.logged-in > li.logged-in-user.dropdown > span').innerHTML.replace(document.querySelector('#topnav > div > div > ul.account-nav.logged-in > li.logged-in-user.dropdown > span').textContent, '')
