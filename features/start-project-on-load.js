9bb15d9a1147c22a12ba4e5e86bcf4f022e83b0b
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-05-20 13:46:35 -0700

    Create start-project-on-load.js

---
+++ features/start-project-on-load.js
@@ -0,0 +1,17 @@
+function startProjectOnLoad() {
+  if (document.querySelector('div.stage_green-flag-overlay_gNXnv') === undefined) {
+    window.setTimeout(startProjectOnLoad, 50)
+  } else {
+    document.querySelector('div.stage_green-flag-overlay_gNXnv').click()
+  }
+}
+
+if (getCookie('ST Features').includes('start-project-on-load')) {
+  startProjectOnLoad()
+}
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
