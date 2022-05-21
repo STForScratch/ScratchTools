0016603284bb20f6b0321585064bb9f8562cf2ed
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-05-20 17:37:33 -0700

    Update sidebar.js

--- features/sidebar.js
+++ features/sidebar.js
@@ -258,3 +258,9 @@ document.querySelector('div#app').prepend(sidebar)
           
         }
       }
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
