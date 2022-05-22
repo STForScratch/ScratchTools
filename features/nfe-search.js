a2a7a6cfbc1d1e1bb67864b063a8aed5093fe918
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-05-22 12:37:57 -0700

    Removed cookie check

--- features/nfe-search.js
+++ features/nfe-search.js
@@ -1,15 +1,8 @@
-// get cookie
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  }
-  // get cookie
+
 
 if (window.location.href.includes("https://scratch.mit.edu/search/")) {
 if (window.location.href.includes('%23')) {
 } else {
-    if (getCookie('ST Features').includes('nfe')) {
 var btn = document.createElement('button')
 btn.className = 'button showAll'
 btn.textContent = 'Show All'
@@ -19,6 +12,5 @@ var apple = str.split('https://scratch.mit.edu/search/projects?q=')[1];
     window.location.href = `https://scratch.mit.edu/search/projects?q=%23${apple.replaceAll('%20', "%20%23")}`
 }
 document.querySelector('#view > div > div > div.sort-controls').appendChild(btn)
-}
 }
 }
