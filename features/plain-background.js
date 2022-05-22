8047f84300a8d1898e22f5ae6be698ee38fc1382
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-05-22 12:38:34 -0700

    Removed cookie check

--- features/plain-background.js
+++ features/plain-background.js
@@ -1,13 +1,6 @@
-// get cookie
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  }
-  // get cookie
+
 if (window.location.href.includes("https://scratch.mit.edu/projects/")) {
     if (window.location.href.includes('editor')) {
-if (getCookie('ST Features').includes('editorbg')) {
     checkforbg()
     function checkforbg() {
         if (document.querySelector('#react-tabs-1 > div.gui_blocks-wrapper_1ccgf.box_box_2jjDp > div > div > svg.blocklySvg > g > rect.blocklyMainBackground') === null) {
@@ -15,7 +8,6 @@ if (getCookie('ST Features').includes('editorbg')) {
         } else {
             document.querySelector('#react-tabs-1 > div.gui_blocks-wrapper_1ccgf.box_box_2jjDp > div > div > svg.blocklySvg > g > rect.blocklyMainBackground').style.fill = 'white'
         }
-    }
 }
     }
 }
