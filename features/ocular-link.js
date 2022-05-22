2b72781df3c48df91556d3b2e22a34782c215c82
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-05-22 13:05:18 -0700

    Create ocular-link.js

---
+++ features/ocular-link.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a').forEach(function(item) {
+  if (item.href.includes('https://scratch.mit.edu/users/')) {
+if (item.className === 'black username') {
+  var test = item.parentNode.cloneNode(true)
+test.firstChild.textContent = 'Open in Ocular'
+test.firstChild.href = `https://ocular.jeffalo.net/users/${item.textContent}/`
+item.parentNode.parentNode.appendChild(test)
+}
+  }
+  })
