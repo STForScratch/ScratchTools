b3c3e3b33895e6899ebb94159beaa6e83d39fcfb
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-06-30 21:01:26 -0700

    Update background.js

--- extras/background.js
+++ extras/background.js
@@ -1,49 +1,52 @@
 chrome.runtime.onInstalled.addListener(() => { // this event is triggered when the extension is installed or reloaded
-    chrome.tabs.create({ url: 'https://tools.scratchstatus.org/' }); // this is to open a webpage
-  });
-  
-  chrome.tabs.onUpdated.addListener(function (tabId , info) {
+    chrome.tabs.create({
+        url: 'https://tools.scratchstatus.org/'
+    }); // this is to open a webpage
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, info) {
     if (info.status === 'loading') {
-  async function getCurrentTab() {
-    var response = await fetch('/features/features.json')
-    var data = await response.json()
-    console.log(data)
-    Object.keys(data).forEach(async function(el) {
-      chrome.storage.sync.get("features", function (obj) {
-        console.log(obj['features']);
-        console.log(obj['features'])
-        if (data[el]['default'] === true) {
-          if (!obj['features'].includes(data[el]['file'])) {
-       chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        files: [`/features/${data[el]['file']}.js`]
-      });
-    }
-        } else {
-        if (obj['features'].includes(data[el]['file'])) {
-       chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        files: [`/features/${data[el]['file']}.js`]
-      });
+        async function getCurrentTab() {
+            var response = await fetch('/features/features.json')
+            var data = await response.json()
+            console.log(data)
+            Object.keys(data).forEach(async function(el) {
+                chrome.storage.sync.get("features", function(obj) {
+                    console.log(obj['features']);
+                    console.log(obj['features'])
+                    if (data[el]['default'] === true) {
+                        if (!obj['features'].includes(data[el]['file'])) {
+                            chrome.tabs.executeScript({
+                                tabId: tabId,
+                                file: `/features/${data[el]['file']}.js`
+                            });
+                        }
+                    } else {
+                        if (obj['features'].includes(data[el]['file'])) {
+                            chrome.tabs.executeScript({
+                                tabId: tabId,
+                                file: `/features/${data[el]['file']}.js`
+                            });
+                        }
+                    }
+                });
+            })
+            var version = '2.5.0'
+            await chrome.storage.sync.get("version", async function(obj) {
+                if (obj['version'] !== version) {
+                    var tab = await chrome.tabs.get(tabId)
+                    if (tab.url.includes('https://scratch.mit.edu')) {
+                        chrome.storage.sync.set({
+                            "version": version
+                        })
+                        chrome.tabs.executeScript({
+                            tabId: tabId,
+                            file: `/extras/new.js`
+                        });
+                    }
+                }
+            })
         }
+        getCurrentTab()
     }
-    });
-    })
-    var version = '2.5.0'
-    await chrome.storage.sync.get("version", async function (obj) {
-        if (obj['version'] !== version) {
-            var tab = await chrome.tabs.get(tabId)
-            if (tab.url.includes('https://scratch.mit.edu')) {
-            chrome.storage.sync.set({"version": version})
-            chrome.scripting.executeScript({
-                target: { tabId: tabId },
-                files: [`/extras/new.js`]
-              });
-            }
-        }
-    })
-  }
-  getCurrentTab()
-  }
-  })
-  
+})
