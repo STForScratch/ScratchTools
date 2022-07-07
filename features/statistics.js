5c859751a7c3b0ab51044c983937d633d333ef1f
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-06 18:03:33 -0700

    Update statistics.js

--- features/statistics.js
+++ features/statistics.js
@@ -5,9 +5,11 @@ el = document.querySelector('#tabs > li:nth-child(5)')
 var clone = el.cloneNode(true);
 document.querySelector('#tabs').appendChild(clone);
 document.querySelector('#tabs > li:nth-child(6) > a').textContent = 'Statistics'
-document.querySelector('#tabs > li:nth-child(6)').className = 'last'
+document.querySelector('#tabs > li:nth-child(6)').className = 'last statistics'
 document.querySelector('#tabs > li:nth-child(6)').dataTab = 'stats'
+  if (document.querySelector('li.statistics') === null) {
 document.querySelector('#tabs > li:nth-child(6) > a').href = `https://scratchstats.com/${document.querySelector('#topnav > div > div > ul.account-nav.logged-in > li.logged-in-user.dropdown > div > ul > li:nth-child(1) > a').href.split('/users/')[1]}`
+}
 }
 setTimeout(() => { stuff() }, 50);
   }
