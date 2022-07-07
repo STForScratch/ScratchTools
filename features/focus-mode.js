b434546c94abbc184d80ab6dc334352b0ddc93c8
Scratchfangs <techgenius64@gmail.com>
2022-07-07 18:23:48 -0400

    Focus Mode

---
+++ features/focus-mode.js
@@ -0,0 +1,7 @@
+if (window.location.href.includes("https://scratch.mit.edu/projects/")) {
+
+  document.querySelector('#footer').remove()
+  document.querySelector('#navigation').remove()
+  document.querySelector("#view > div > div.project-lower-container").remove()
+
+}
