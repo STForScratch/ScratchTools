c3236f2f31afdcea8c71a2a2c1e42d6c3de3f849
Scratchfangs <techgenius64@gmail.com>
2022-07-08 09:31:37 -0400

    Update focus-mode.js

--- features/focus-mode.js
+++ features/focus-mode.js
@@ -1,5 +1,13 @@
 if (window.location.href.includes("https://scratch.mit.edu/projects/")) {
-
+checkSubactions()
+function checkSubactions() {
+    if(document.querySelector('#view > div > div.inner > div:nth-child(3) > div.flex-row.subactions') === null) {
+        window.setTimeout(checkSubactions, 100)
+    } else {
+        focusMode()
+    }
+}
+function focusMode() {
 var btn = document.createElement('button')
 btn.className = 'button focusMode'
 btn.textContent = 'Focus Mode'
@@ -9,4 +17,5 @@ btn.onclick = function() {
   document.querySelector("#view > div > div.project-lower-container").remove()
 }
 document.querySelector("#view > div > div.inner > div:nth-child(3) > div.flex-row.subactions").appendChild(btn)
+}
 }
