6432a4277199fdf10a5546ec76377dbf1e793d65
Scratchfangs <techgenius64@gmail.com>
2022-07-08 09:15:39 -0400

    Update focus-mode.js

--- features/focus-mode.js
+++ features/focus-mode.js
@@ -1,7 +1,12 @@
 if (window.location.href.includes("https://scratch.mit.edu/projects/")) {
 
+var btn = document.createElement('button')
+btn.className = 'button focusMode'
+btn.textContent = 'Focus Mode'
+btn.onclick = function() {
   document.querySelector('#footer').remove()
   document.querySelector('#navigation').remove()
   document.querySelector("#view > div > div.project-lower-container").remove()
-
+}
+document.querySelector("#view > div > div.inner > div:nth-child(3) > div.flex-row.subactions").appendChild(btn)
 }
