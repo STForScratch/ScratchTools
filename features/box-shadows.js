3762c6f9362834cb449a0fd0bfae87ccb79cadfd
Scratchfangs <techgenius64@gmail.com>
2022-07-10 14:25:52 -0400

    Create box-shadows.js

---
+++ features/box-shadows.js
@@ -0,0 +1,10 @@
+var style = document.createElement('style')
+style.innerHTML = `
+    .box {
+transition: box-shadow .3s;
+}
+  .box:hover {
+    box-shadow: 0 0 5px #999999;
+  }
+`
+document.body.appendChild(style)
