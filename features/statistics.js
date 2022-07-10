5608cbf9ad21cb833a0a10f3ec682f0426e33381
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-10 08:12:58 -0700

    Fix trash duplication bug

--- features/statistics.js
+++ features/statistics.js
@@ -1,5 +1,6 @@
 
   if (window.location.href.includes("https://scratch.mit.edu/mystuff")) {
+    if (document.querySelector('li.statistics') === null) {
 function stuff() {
 el = document.querySelector('#tabs > li:nth-child(5)')
 var clone = el.cloneNode(true);
@@ -7,9 +8,8 @@ document.querySelector('#tabs').appendChild(clone);
 document.querySelector('#tabs > li:nth-child(6) > a').textContent = 'Statistics'
 document.querySelector('#tabs > li:nth-child(6)').className = 'last statistics'
 document.querySelector('#tabs > li:nth-child(6)').dataTab = 'stats'
-  if (document.querySelector('li.statistics') === null) {
 document.querySelector('#tabs > li:nth-child(6) > a').href = `https://scratchstats.com/${document.querySelector('#topnav > div > div > ul.account-nav.logged-in > li.logged-in-user.dropdown > div > ul > li:nth-child(1) > a').href.split('/users/')[1]}`
 }
-}
 setTimeout(() => { stuff() }, 50);
+    }
   }
