71c082777fc1f40105c7c5c45e4d2eb83529a9f9
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-10 07:48:31 -0700

    Update aviate.js

--- features/aviate.js
+++ features/aviate.js
@@ -1,52 +1,16 @@
-var style = document.createElement('style')
-if (window.location.href.includes('https://scratch.mit.edu/messages')) {
-style.innerHTML = `
-  .social-message
-  {
-    border-bottom-color: #C5C5C5;
-  }
-
-  .social-message.mod-forum-activity
-  {
-    background-color: hsl(300, 55%, 92%);
-  }
-
-  .social-message.mod-comment-message,
-  .social-message.mod-follow-user
-  {
-    background-color: hsl(215, 100%, 90%);
-  }
-
-  .social-message.mod-curator-invite,
-  .social-message.mod-become-manager,
-  .social-message.mod-studio-activity
-  {
-    background-color: hsl(163, 60%, 85%);
-  }
-
-  .social-message.mod-love-favorite
-  {
-    background-color: hsl(45, 90%, 95%);
-  }
-
-  .social-message.mod-love-project
-  {
-    background-color: hsl(350, 100%, 95%);
-  }
-
-  .social-message.mod-remix-project
-  {
-    background-color: hsl(260, 100%, 90%);
-  }
-
-.social-message {
-opacity: 0.75;
-}
-
-  .social-message.mod-unread
-  {
-    opacity: 1;
-  }
-`
-document.body.appendChild(style)
-}
+async function getTheUser() {
+    var user = window.location.href.replace('https://scratch.mit.edu/users/', '').replaceAll('/', '')
+    var response = await fetch('https://aviateapp.eu.org/api/'+user)
+    var data = await response.json()
+    if (data.success === true && data.status !== null) {
+    if (document.querySelector('i.aviate.scratchtools') === null) {
+    var i = document.createElement('i')
+    i.textContent = data.status
+    i.style.opacity = '0.5'
+    i.title = "This is an Aviate Status being displayed with ScratchTools."
+    i.className = 'aviate scratchtools'
+    document.querySelector('div.header-text').appendChild(i)
+    }
+    }
+    }
+    getTheUser()
