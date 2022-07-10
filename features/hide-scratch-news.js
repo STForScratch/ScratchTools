396184afa3670ddb267f4344a57f71c7c0a555c7
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-10 09:29:36 -0700

    Create hide-scratch-news.js

---
+++ features/hide-scratch-news.js
@@ -0,0 +1,4 @@
+if (window.location.href === 'https://scratch.mit.edu' || window.location.href === 'https://scratch.mit.edu/') {
+document.querySelector('div.box.news').remove()
+document.querySelector('div.box.activity').style.width = 'calc(120% - 20px)'
+}
