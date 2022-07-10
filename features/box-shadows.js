319088fe63dec4e632a7223d072b3c0f47123fef
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-10 11:53:04 -0700

    Update box-shadows.js

--- features/box-shadows.js
+++ features/box-shadows.js
@@ -1,10 +1,11 @@
 var style = document.createElement('style')
 style.innerHTML = `
     .box {
-transition: box-shadow .1s;
+transition: box-shadow .2s;
+box-shadow: 0 0 7px #999999;
 }
   .box:hover {
-    box-shadow: 0 0 5px #999999;
+    box-shadow: 0 0 15px #999999;
   }
 `
 document.body.appendChild(style)
