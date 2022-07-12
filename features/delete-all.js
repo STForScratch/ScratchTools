d466b741e67e48e843b9e54685168c40eae69f9a
Scratchfangs <techgenius64@gmail.com>
2022-07-12 10:43:50 -0400

    Update delete-all.js

--- features/delete-all.js
+++ features/delete-all.js
@@ -1,31 +1,30 @@
 function checkForContextMenu() {
-    if (document.querySelector('.scratchtools.deleteall') === null && document.querySelector('nav.react-contextmenu.react-contextmenu--visible') !== null) {
-        var div = document.createElement('div')
-        div.className = 'react-contextmenu-item context-menu_menu-item_3cioN context-menu_menu-item-bordered_29CJG context-menu_menu-item-danger_1tJg0 scratchtools deleteall'
-        div.role = 'menuitem'
-        div.tabindex = '-1'
-        div.arialDisabled = 'false'
-        div.innerHTML = `<span>delete all</span>`
-        document.querySelector('nav.react-contextmenu.react-contextmenu--visible').appendChild(div)
-        div.onclick = function() {
-            var shouldI = confirm("Are you sure you want to delete all sprites in the project?")
-            if (shouldI) {
-                deleteAllSprites()
-            }
-        }
-    } else {
-    }
-    setTimeout(checkForContextMenu, 200)
+	if (document.querySelector('.scratchtools.deleteall') === null && document.querySelector('nav.react-contextmenu.react-contextmenu--visible') !== null) {
+		var div = document.createElement('div')
+		div.className = 'react-contextmenu-item context-menu_menu-item_3cioN context-menu_menu-item-bordered_29CJG context-menu_menu-item-danger_1tJg0 scratchtools deleteall'
+		div.role = 'menuitem'
+		div.tabindex = '-1'
+		div.arialDisabled = 'false'
+		div.innerHTML = `<span>delete all</span>`
+		document.querySelector('nav.react-contextmenu.react-contextmenu--visible').appendChild(div)
+		div.onclick = function() {
+			var shouldI = confirm("Are you sure you want to delete all sprites in the project?")
+			if (shouldI) {
+				deleteAllSprites()
+			}
+		}
+	} else {}
+	setTimeout(checkForContextMenu, 200)
 }
 checkForContextMenu()
 
 function deleteAllSprites() {
-    document.querySelectorAll('div').forEach(function(el) {
-    if (el.className.includes('sprite-selector-item_sprite-name_')) {
-vm.deleteSprite(vm.runtime.getSpriteTargetByName(el.textContent).id)
-    }
-})
+	document.querySelectorAll('div').forEach(function(el) {
+		if (el.className.includes('sprite-selector-item_sprite-name_')) {
+			vm.deleteSprite(vm.runtime.getSpriteTargetByName(el.textContent).id)
+		}
+	})
 }
 if (window.location.href.includes('https://scratch.mit.edu/projects/') && window.location.href.includes('/editor')) {
-checkForContextMenu()
+	checkForContextMenu()
 }
