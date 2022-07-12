94acdea6f3e33244b8271160164dbc1975a70ecd
Scratchfangs <techgenius64@gmail.com>
2022-07-12 10:49:16 -0400

    Update project-links.js

--- features/project-links.js
+++ features/project-links.js
@@ -1,53 +1,57 @@
 if (window.location.href.includes('https://scratch.mit.edu/users/')) {
-replacealllinks()
-const targetNode = document.querySelector('ul.comments');
+	replacealllinks()
+	const targetNode = document.querySelector('ul.comments');
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
+	// Options for the observer (which mutations to observe)
+	const config = {
+		attributes: true,
+		childList: true,
+		subtree: true
+	};
 
-// Callback function to execute when mutations are observed
-const callback = function(mutationList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for(const mutation of mutationList) {
-        if (mutation.type === 'childList') {
-            replacealllinks()
-        }
-    }
-};
+	// Callback function to execute when mutations are observed
+	const callback = function(mutationList, observer) {
+		// Use traditional 'for loops' for IE 11
+		for (const mutation of mutationList) {
+			if (mutation.type === 'childList') {
+				replacealllinks()
+			}
+		}
+	};
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+	// Create an observer instance linked to the callback function
+	const observer = new MutationObserver(callback);
 
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+	// Start observing the target node for configured mutations
+	observer.observe(targetNode, config);
 
 
-  function replacealllinks() {
-  const highlightedItems = document.querySelector('ul.comments').querySelectorAll("a");
-  
-  highlightedItems.forEach(function(item) {
-      if (item.href.includes('https://scratch.mit.edu/projects/')) {
-  if (!item.className.includes('scratchtools')) {
-      item.className = item.className+' scratchtools'
-  replacelinks(item)
-  }
-      }
-  });
-  
-  async function replacelinks(item) {
-      
-      // Storing response
-      const response = await fetch(`https://api.${item.href.replace('https://', '')}`);
-      
-      // Storing data in form of JSON
-      var data = await response.json();
-      console.log(data);
-  if(data.hasOwnProperty('title')){
-      var stuff = data["title"]
-  item.textContent = stuff
-  }
-      }
+	function replacealllinks() {
+		const highlightedItems = document.querySelector('ul.comments').querySelectorAll("a");
 
-    }
+		highlightedItems.forEach(function(item) {
+			if (item.href.includes('https://scratch.mit.edu/projects/')) {
+				if (!item.className.includes('scratchtools')) {
+					item.className = item.className + ' scratchtools'
+					replacelinks(item)
+				}
+			}
+		});
 
-  }
+		async function replacelinks(item) {
+
+			// Storing response
+			const response = await fetch(`https://api.${item.href.replace('https://', '')}`);
+
+			// Storing data in form of JSON
+			var data = await response.json();
+			console.log(data);
+			if (data.hasOwnProperty('title')) {
+				var stuff = data["title"]
+				item.textContent = stuff
+			}
+		}
+
+	}
+
+}
