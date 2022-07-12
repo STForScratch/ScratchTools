289d3f5e9394eface969d984e701b8515c52a9bb
Scratchfangs <techgenius64@gmail.com>
2022-07-12 10:50:40 -0400

    Update round-profile-pictures.js

--- features/round-profile-pictures.js
+++ features/round-profile-pictures.js
@@ -1,12 +1,12 @@
 function roundProfile() {
-    document.querySelectorAll('img').forEach(function(el) {
-        if (el.src !== undefined) {
-            if (el.src.includes('scratch.mit.edu/get_image/user/')) {
-                el.style.borderRadius = '50%'
-            }
-        }
-    })
-    window.setTimeout(roundProfile, 80)
+	document.querySelectorAll('img').forEach(function(el) {
+		if (el.src !== undefined) {
+			if (el.src.includes('scratch.mit.edu/get_image/user/')) {
+				el.style.borderRadius = '50%'
+			}
+		}
+	})
+	window.setTimeout(roundProfile, 80)
 }
 
 roundProfile()
