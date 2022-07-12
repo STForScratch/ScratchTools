dc2ddd4877e152c9befc847fb0f0b71419e51b0c
Scratchfangs <techgenius64@gmail.com>
2022-07-12 10:50:53 -0400

    Update sprite-data.js

--- features/sprite-data.js
+++ features/sprite-data.js
@@ -1,41 +1,40 @@
 function getSpriteData() {
-    try {
-    var currentSpriteName = []
-    document.querySelectorAll('div').forEach(function(el) {
-if (el.className.includes('sprite-selector-item_is-selected')) {
-el.querySelectorAll('div').forEach(function(el) {
-if (el.className.includes('sprite-selector-item_sprite-name_')) {
-    currentSpriteName.push(el.textContent)
-}
-})
-    }
-})
-if (currentSpriteName.length !== 0) {
-var currentSpriteName = currentSpriteName[0]
-try {
-document.querySelector('#react-tabs-0 > span').textContent = `Code (${Object.keys(vm.runtime.getSpriteTargetByName(currentSpriteName).blocks._blocks).length})`
-} catch(err) {
+	try {
+		var currentSpriteName = []
+		document.querySelectorAll('div').forEach(function(el) {
+			if (el.className.includes('sprite-selector-item_is-selected')) {
+				el.querySelectorAll('div').forEach(function(el) {
+					if (el.className.includes('sprite-selector-item_sprite-name_')) {
+						currentSpriteName.push(el.textContent)
+					}
+				})
+			}
+		})
+		if (currentSpriteName.length !== 0) {
+			var currentSpriteName = currentSpriteName[0]
+			try {
+				document.querySelector('#react-tabs-0 > span').textContent = `Code (${Object.keys(vm.runtime.getSpriteTargetByName(currentSpriteName).blocks._blocks).length})`
+			} catch (err) {
 
-}
-try {
-    document.querySelector('#react-tabs-2 > span').textContent = `Costumes (${vm.runtime.getSpriteTargetByName(currentSpriteName).getCostumes().length})`
-} catch(err) {
+			}
+			try {
+				document.querySelector('#react-tabs-2 > span').textContent = `Costumes (${vm.runtime.getSpriteTargetByName(currentSpriteName).getCostumes().length})`
+			} catch (err) {
 
-}
-try {
-    document.querySelector('#react-tabs-4 > span').textContent = `Sounds (${vm.runtime.getSpriteTargetByName(currentSpriteName).getSounds().length})`
-} catch(err) {
+			}
+			try {
+				document.querySelector('#react-tabs-4 > span').textContent = `Sounds (${vm.runtime.getSpriteTargetByName(currentSpriteName).getSounds().length})`
+			} catch (err) {
 
-}
-} else {
-document.querySelector('#react-tabs-0 > span').textContent = `Code`
-    document.querySelector('#react-tabs-2 > span').textContent = `Backdrops`
-    document.querySelector('#react-tabs-4 > span').textContent = `Sounds`
-}
-    } catch(err) {
-    }
-setTimeout(getSpriteData, 200)
+			}
+		} else {
+			document.querySelector('#react-tabs-0 > span').textContent = `Code`
+			document.querySelector('#react-tabs-2 > span').textContent = `Backdrops`
+			document.querySelector('#react-tabs-4 > span').textContent = `Sounds`
+		}
+	} catch (err) {}
+	setTimeout(getSpriteData, 200)
 }
 if (window.location.href.includes('https://scratch.mit.edu/projects/') && window.location.href.includes('/editor')) {
-getSpriteData()
+	getSpriteData()
 }
