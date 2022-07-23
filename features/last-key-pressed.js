a01c413be53f86e54ddd957b222f03ac36ab1a8e
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-23 12:55:10 -0700

    Create last-key-pressed.js

---
+++ features/last-key-pressed.js
@@ -0,0 +1,29 @@
+function addKeyPressed() {
+  var lkp = document.createElement('div')
+  lkp.textContent = `No Key Pressed`
+  lkp.className = 'scratchtools key share-date'
+  if (document.querySelector('div.scratchtools.key') === null) {
+  document.querySelector('div.flex-row.subactions').prepend(lkp)
+      vm.runtime.on('KEY_PRESSED', function(el) {
+      document.querySelector('div.scratchtools.key').textContent = 'Last Key Pressed: '+el
+  })
+  }
+}
+
+const page = document.querySelector('div.page');
+const configure = {
+    attributes: true,
+    childList: true,
+    subtree: true
+};
+const getSpot = function(mutationList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for (const mutation of mutationList) {
+        if (document.querySelector("div.flex-row.subactions") !== null) {
+            observer.disconnect()
+                addKepPressed()
+        }
+    }
+};
+const observer = new MutationObserver(getSpot);
+observer.observe(page, configure);
