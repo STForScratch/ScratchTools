dc07fcc284d29c878cb8ef661ca0116be3d7a58e
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-24 15:00:19 -0700

    Update main.js

--- api/main.js
+++ api/main.js
@@ -1,11 +1,7 @@
 var ScratchTools = {}
 console.log("ScratchTools API Created")
-var editor = window.vm || (() => {
-    const app = document.querySelector("#app");
-    return app[Object.keys(app).find(key => key.startsWith("__reactContainer"))].child.stateNode.store.getState().scratchGui.vm;
-})();
-if (editor === undefined) {
-  ScratchTools.type = 'Website'
+if (window.location.href.startsWith('https://scratch.mit.edu/projects/') && window.location.href.includes('/editor')) {
+  ScratchTools.type = 'Editor'
 } else {
- ScratchTools.type = 'Editor' 
+ ScratchTools.type = 'Website' 
 }
