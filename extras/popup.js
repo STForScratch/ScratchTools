e7042d4881cc447edcefef1e4a54e591a2393c9a
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-23 17:08:46 -0700

    Update popup.js

--- extras/popup.js
+++ extras/popup.js
@@ -393,24 +393,3 @@ async function getFeaturesBySearch(search) {
 document.querySelector('h2.feedback').onclick = function() {
     chrome.tabs.create({ url:"https://tools.scratchstatus.org/feedback/" })
 }
-
-if (chrome.runtime.getManifest().version_name.toLowerCase().includes('beta')) {
-    var style = document.createElement('style')
-    style.innerHTML = `
-    input:checked+.slider {
-    background-color: #6f00ff;
-    float: right;
-}
-
-input:focus+.slider {
-    box-shadow: 0 0 1px #6f00ff;
-    float: right;
-}
-
-.featureTitle {
-    color: #6f00ff;
-}
-    `
-    document.body.appendChild(style)
-}
-console.log(chrome.runtime.getManifest().version_name.toLowerCase())
