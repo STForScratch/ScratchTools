73e7b42ab0725a81e498714abc8890c5ec0dd408
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-24 15:01:15 -0700

    Update vm.js

--- api/vm.js
+++ api/vm.js
@@ -21,9 +21,5 @@ logTool("Loaded VM.")
     warnTool("Could not load Blockly.")
 }
 }
-var vm = window.vm || (() => {
-        const app = document.querySelector("#app");
-        return app[Object.keys(app).find(key => key.startsWith("__reactContainer"))].child.stateNode.store.getState().scratchGui.vm;
-    })();
 
 ScratchTools.Scratch = {"vm":vm, "blockly":Blockly}
