12ed68b89a5441ad951a7af9efb97b93f645bf76
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-24 08:21:02 -0700

    Update beta.js

--- extras/beta.js
+++ extras/beta.js
@@ -28,7 +28,7 @@ a:hover, a:visited, a:link, a:active {
     `
     document.body.appendChild(style)
     if (document.querySelector('h2.title') !== null) {
-        document.querySelector('h2.title').innerHTML = `ScratchTools Settings <span style="color: #6f00ff">Beta</span>`
+        document.querySelector('h2.title').innerHTML = `ScratchTools Settings <span style="color: #9b57fa">Beta</span>`
     }
     if (document.querySelector('div.span') !== null) {
         document.querySelector('div.span').textContent = 'ScratchTools Beta '   
