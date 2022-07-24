01eeb52ed984fdb9eefdea64004214a0e9f1049e
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-24 15:10:56 -0700

    Update vm.js

--- api/vm.js
+++ api/vm.js
@@ -3,22 +3,22 @@ var vm = window.vm || (() => {
     const app = document.querySelector("#app");
     return app[Object.keys(app).find(key => key.startsWith("__reactContainer"))].child.stateNode.store.getState().scratchGui.vm;
 })();
-logTool("Loaded VM.")
+ScratchTools.console.log("Loaded VM.")
     if (Blockly !== undefined) {
-        logTool("Loaded Blockly.")
+        ScratchTools.console.log("Loaded Blockly.")
     } else {
-        warnTool("Could not load Blockly.")
+        ScratchTools.console.warn("Could not load Blockly.")
     }
 } catch(err) {
-    warnTool("Could not load VM.")
+    ScratchTools.console.warn("Could not load VM.")
     try {
     if (Blockly === undefined) {
-        logTool("Loaded Blockly.")
+        ScratchTools.console.log("Loaded Blockly.")
     } else {
-        warnTool("Could not load Blockly.")
+        ScratchTools.console.warn("Could not load Blockly.")
     }
 } catch(err) {
-    warnTool("Could not load Blockly.")
+    ScratchTools.console.warn("Could not load Blockly.")
 }
 }
 ScratchTools.Scratch = {}
@@ -27,7 +27,7 @@ if (Blockly !== undefined) {
     ScratchTools.Scratch.blockly = Blockly   
 }
 } catch(err) {
-    warnTool("Tried to load Blockly but failed.")   
+    ScratchTools.console.warn("Tried to load Blockly but failed.")   
 }
 if (vm !== undefined) {
     ScratchTools.Scratch.vm = vm   
