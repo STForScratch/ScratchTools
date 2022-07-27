78c81fbe9a2ce8888f4929bd708e02861e8e6045
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-07-27 08:59:28 -0700

    Update remove-collapse-buttons.js

--- features/remove-collapse-buttons.js
+++ features/remove-collapse-buttons.js
@@ -1,1 +1,3 @@
-document.querySelectorAll(".toggle").forEach(el => el.remove());
+if (window.location.href.startsWith('https://scratch.mit.edu/discuss/')) {
+  document.querySelectorAll(".toggle").forEach(el => el.remove());
+}
