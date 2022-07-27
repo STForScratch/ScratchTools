40d68294061da882f30d14cef53ef10f5aeee87a
Scratchfangs <techgenius64@gmail.com>
2022-07-27 13:15:17 -0400

    Create remove-topic-and-post.js

---
+++ features/remove-topic-and-post.js
@@ -0,0 +1,4 @@
+if (window.location.href.endsWith('https://scratch.mit.edu/discuss/')) {
+    document.querySelectorAll(".tc2").forEach(el => el.remove())
+    document.querySelectorAll(".tc3").forEach(el => el.remove())
+}
