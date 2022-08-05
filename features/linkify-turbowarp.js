014f8070283c80825e5c97fa25b3ede742259d74
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-08-05 10:50:37 -0700

    Fix content locating function

--- features/linkify-turbowarp.js
+++ features/linkify-turbowarp.js
@@ -1,3 +1,3 @@
-document.querySelector("body").innerHTML = document.querySelector("body").innerHTML.replaceAll(
+document.querySelectorAll(".content").forEach(el => e.innerText = e.innerText.replaceAll(
   /[^>"'](http|https)?:\/\/turbowarp\.org\/#?[0-9]*\/?(?:editor)?/ig/* This is a regex to find turbowarp uris that aren't links*/,
-  "<a href=$&>$&</a>");
+  "<a href=$&>$&</a>"));
