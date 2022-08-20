18c6e99bf2feaaac7c97ab53d3f426de88d1251b
misty <93560458+mistysloth@users.noreply.github.com>
2022-08-19 22:18:59 -0400

    Delete the-forbidden-secret.js

--- features/the-forbidden-secret.js
+++
@@ -1,3 +0,0 @@
-if (getCookie('ST Features').includes('secret')) {
-(function(){['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){document.body.style[prefix + 'transform'] = 'rotate(180deg)';});}());
-}
