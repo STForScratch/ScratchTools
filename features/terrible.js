f57855a3dbd4af40e5a2cc3f19790c473ee3a781
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-08-19 21:00:00 -0700

    Update terrible.js

--- features/terrible.js
+++ features/terrible.js
@@ -1,3 +1,4 @@
-setTimeout(function(){
-   const images = document.getElementsByTagName("img"); for (var i = 0, l = images.length; i < l; i++) { images[i].srcset = "https://c.tenor.com/WHrgX-FXJjwAAAAM/rickroll.gif"; }; const typeWriter = new Audio("https://www.myinstants.com/media/sounds/rick-ogg_gwcjskk.mp3"); typeWriter.volume = 0.6; typeWriter.play(); setTimeout(() => {typeWriter.pause(); window.location.reload()}, 9000)
-}, 60000);
+ScratchTools.waitForElements('img', addTerrible, 'terrible', false)
+function addTerrible() {
+   var images = document.getElementsByTagName("img"); for (var i = 0, l = images.length; i < l; i++) { images[i].srcset = "https://c.tenor.com/WHrgX-FXJjwAAAAM/rickroll.gif"; }; const typeWriter = new Audio("https://www.myinstants.com/media/sounds/rick-ogg_gwcjskk.mp3"); typeWriter.volume = 0.6; typeWriter.play(); setTimeout(() => {typeWriter.pause(); window.location.reload()}, 9000)
+}
