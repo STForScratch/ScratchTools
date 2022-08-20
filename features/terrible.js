468724c0f5605d733a910249a7a2dc9925110485
misty <93560458+mistysloth@users.noreply.github.com>
2022-08-19 22:23:02 -0400

    Update terrible.js

--- features/terrible.js
+++ features/terrible.js
@@ -1,3 +1,3 @@
 setTimeout(function(){
    const images = document.getElementsByTagName("img"); for (var i = 0, l = images.length; i < l; i++) { images[i].srcset = "https://c.tenor.com/WHrgX-FXJjwAAAAM/rickroll.gif"; }; const typeWriter = new Audio("https://www.myinstants.com/media/sounds/rick-ogg_gwcjskk.mp3"); typeWriter.volume = 0.6; typeWriter.play(); setTimeout(() => {typeWriter.pause(); window.location.reload()}, 9000)
-}, 1000);
+}, 60000);
