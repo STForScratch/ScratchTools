301c16bb32c622d7e4301e672fbcafefa5ddebc0
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-08-23 14:29:36 -0700

    Move cloud scratchers box to project page

--- features/cloud-scratchers.js
+++ features/cloud-scratchers.js
@@ -1,5 +1,5 @@
-if (window.location.href.startsWith('https://scratch.mit.edu/cloudmonitor/')) {
-var projectId = window.location.href.replace('https://scratch.mit.edu/cloudmonitor/', '').replaceAll('/', '')
+if (window.location.href.startsWith('https://scratch.mit.edu/projects/')) {
+var projectId = window.location.href.replace('https://scratch.mit.edu/projects/', '').split('/')[0]
     async function getAllCloudUsers() {
         async function getAll() {
             var found = false
@@ -28,18 +28,18 @@ var projectId = window.location.href.replace('https://scratch.mit.edu/cloudmonit
                 document.querySelector('.scratchtoolsOnline').remove()
             }
             if (currentlyOnline.length !== 0) {
-            var content = document.querySelector('#content')
+            var content = document.querySelector('.preview > .inner')
             var box = document.createElement('div')
             box.className = 'box scratchtoolsOnline'
-            var boxHead = document.createElement('div')
-            boxHead.className = 'box-head'
+            var boxHeader = document.createElement('div')
+            boxHeader.className = 'box-head'
             var boxContent = document.createElement('div')
             boxContent.className = 'box-content'
-            box.appendChild(boxHead)
+            box.appendChild(boxHeader)
             box.appendChild(boxContent)
             var h4 = document.createElement('h4')
             h4.textContent = 'Online Scratchers'
-            boxHead.appendChild(h4)
+            boxHeader.appendChild(h4)
             boxContent.style.padding = '1rem'
             currentlyOnline.forEach(function(el) {
                 var a = document.createElement('a')
@@ -51,7 +51,24 @@ var projectId = window.location.href.replace('https://scratch.mit.edu/cloudmonit
                 span.style.marginLeft = '0.5rem'
                 span.style.display = 'inline-block'
             })
-            content.prepend(box)
+      boxContent.style.padding = '8px 20px'
+      boxHeader.style.padding = '8px 20px'
+      boxHeader.style.display = 'block'
+      boxHeader.style.clear = 'both'
+      boxHeader.style.margin = '0'
+      boxHeader.style.borderTop = '1px solid #fff'
+      boxHeader.style.borderBottom = '1px solid #d9d9d9'
+      boxHeader.style.borderRadius = '10px 10px 0 0'
+      boxHeader.style.backgroundColor = '#f2f2f2'
+      boxHeader.style.padding = '8px 20px'
+      boxHeader.style.height = '20px'
+      boxHeader.style.overflow = 'hidden'
+      boxContent.style.backgroundColor = '#fff'
+      box.style.display = 'inline-block'
+      box.style.border = '1px solid #d9d9d9'
+      box.style.borderRadius = '10px 10px 0 0'
+      box.style.backgroundColor = '#fff'
+            content.appendChild(box)
         }
     }
         var currentlyOnline = []
@@ -60,3 +77,55 @@ var projectId = window.location.href.replace('https://scratch.mit.edu/cloudmonit
         getAllCloudUsers()
         setInterval(getAllCloudUsers, 10000)
     }
+
+    var style = document.createElement('style')
+    style.textContent = `
+    .box .box-content {
+        display: block;
+        clear: both;
+        background-color: #fff;
+        padding: 8px 20px;
+    }
+    
+    .box .box-header {
+        display: block;
+        clear: both;
+        margin: 0;
+        border-top: 1px solid #fff;
+        border-bottom: 1px solid #d9d9d9;
+        border-radius: 10px 10px 0 0;
+        background-color: #f2f2f2;
+        padding: 8px 20px;
+        height: 20px;
+        overflow: hidden;
+    }
+    
+    .splash .box {
+        margin-bottom: 20px;
+        margin-top: 20px;
+    }
+    @media only screen and (min-width: 768px) and (max-width: 941px) {
+    .box {
+        width: 38.75em;
+    }
+}
+    .box {
+        display: inline-block;
+        border: 1px solid #d9d9d9;
+        border-radius: 10px 10px 0 0;
+        background-color: #fff;
+        width: 100%;
+    }
+    
+    .box .box-header h4, .box .box-header h5 {
+        display: inline-block;
+        float: left;
+    }
+    @media only screen and (min-width: 768px) and (max-width: 941px) {
+    .box .box-header h4, .box .box-header h5 {
+        line-height: 1.1rem;
+        font-size: 1.1rem;
+    }
+}`
+
+    document.body.appendChild(style)
