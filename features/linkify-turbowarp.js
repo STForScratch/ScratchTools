909ccb638b42127995d2638180a6c391be576c77
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-08-25 10:53:14 -0700

    Update linkify-turbowarp.js

--- features/linkify-turbowarp.js
+++ features/linkify-turbowarp.js
@@ -1,6 +1,6 @@
 ScratchTools.waitForElements(".content", () => {
 document.querySelectorAll(".content").forEach(el => e.innerText = e.innerText.replaceAll(
   /[^>"'](http|https)?:\/\/turbowarp\.org\/#?[0-9]*\/?(?:editor)?/ig/* This is a regex to find turbowarp uris that aren't links*/,
-  `<a href=$&>$&</a>`));
+  `<a href=${RegExp.$&}>${RegExp.$&}</a>`));
   }
   );
