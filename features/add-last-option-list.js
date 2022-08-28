af29e1510373df67ade25d01f2cb87688e7a7eec
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-08-27 20:10:01 -0700

    Create add-last-option-list.js

---
+++ features/add-last-option-list.js
@@ -0,0 +1,18 @@
+if (window.location.href.startsWith('https://scratch.mit.edu/projects/') && window.location.href.includes('/editor')) {
+  function addLastOptionToBlock() {
+    Object.keys(Blockly.getMainWorkspace().blockDB_).forEach(function(el) {
+        var block = Blockly.getMainWorkspace().getBlockById(el)
+        if (block.type === "data_itemoflist") {
+            function addLastItem(el) {
+                var last = {"enabled":true, "text":"Select Last"}
+        last.callback = function() {
+            block.childBlocks_[0].inputList[0].fieldRow[0].setText('last')
+        }
+                el.push(last)
+            }
+            ScratchTools.Scratch.waitForContextMenu({"block":block.id,"id":"set last item","callback":addLastItem})
+        }
+    })
+  }
+  ScratchTools.waitForElements('.blocklyDraggable', addLastOptionToBlock, 'addLastOptionToBlock', false)
+}
