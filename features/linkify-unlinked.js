fb819ea0a2f2531bc1d5060c9203689e6ad77eff
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-08-29 12:20:42 -0700

    change target

--- features/linkify-unlinked.js
+++ features/linkify-unlinked.js
@@ -1,6 +1,6 @@
-ScratchTools.waitForElements(".content", () => {
-document.querySelectorAll(".content").forEach(el => e.innerText = e.innerText.replaceAll(
+ScratchTools.waitForElements("comment-container", function(){
+document.querySelectorAll("comment-container").forEach(el => e.innerText = e.innerText.replaceAll(
   /[^>"'](https?:\/\/[A-Za-z0-9\.-]+\.[a-zA-Z])/ig/* This is a regex to find uris that aren't links*/,
   `<a href=${RegExp['$&']}>${RegExp['$&']}</a>`));
-  }
+  }, 
   );
