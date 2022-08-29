bdb8207b1536f21c92b799c29d4f305529b20615
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-08-29 10:30:51 -0700

    fix bug

--- features/linkify-unlinked.js
+++ features/linkify-unlinked.js
@@ -1,6 +1,6 @@
 ScratchTools.waitForElements(".content", () => {
 document.querySelectorAll(".content").forEach(el => e.innerText = e.innerText.replaceAll(
   /[^>"'](https?:\/\/[A-Za-z0-9\.-]+\.[a-zA-Z])/ig/* This is a regex to find uris that aren't links*/,
-  `<a href=${RegExp.$&}>${RegExp.$&}</a>`));
+  `<a href=${RegExp['$&']}>${RegExp['$&']}</a>`));
   }
   );
