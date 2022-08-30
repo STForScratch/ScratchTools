a630c8117c8152bf9541d7d0b83fc02dee0dc455
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-08-30 09:00:33 -0700

    does this work?

--- features/linkify-unlinked.js
+++ features/linkify-unlinked.js
@@ -2,5 +2,5 @@ ScratchTools.waitForElements("comment-container", function(){
 document.querySelectorAll("comment-container").forEach(e => e.innerText = e.innerText.replaceAll(
   /[^>"'](https?:\/\/[A-Za-z0-9\.-]+\.[a-zA-Z])/ig/* This is a regex to find uris that aren't links*/,
   `<a href=${RegExp['$&']}>${RegExp['$&']}</a>`));
-  }, 
+  }, "linkifyunlinked"
   );
