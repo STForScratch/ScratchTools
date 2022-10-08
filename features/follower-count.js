4bda6006bff777e3a04f40ab22004cc5997b809a
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-10-08 08:00:24 -0700

    Fix follower count appearing on other pages

--- features/follower-count.js
+++ features/follower-count.js
@@ -1,36 +1,44 @@
-async function getFollowerCount(el) {
-  var response = await fetch(
-    "https://scratch.mit.edu/users/"+el.querySelector('a').textContent+"/followers/"
-  );
-  var data = (await response.text())
+if (
+  window.location.href
     .toLowerCase()
-    .replaceAll("<!doctype html>", "");
-  var div = document.createElement("div");
-  div.innerHTML = data;
-  if (!document.querySelector(".scratchtoolsFollowerCount")) {
-    if (stillComplete) {
-      var span = document.createElement("span");
-      span.textContent =
-        " (" +
-        div
-          .querySelector(".box-head > h2")
-          .textContent.split("(")[1]
-          .split(")")[0] +
-        " followers" +
-        ")";
-      span.className = "scratchtoolsFollowerCount";
-      document.querySelector(".title").appendChild(span);
+    .startsWith("https://scratch.mit.edu/projects/")
+) {
+  async function getFollowerCount(el) {
+    var response = await fetch(
+      "https://scratch.mit.edu/users/" +
+        el.querySelector("a").textContent +
+        "/followers/"
+    );
+    var data = (await response.text())
+      .toLowerCase()
+      .replaceAll("<!doctype html>", "");
+    var div = document.createElement("div");
+    div.innerHTML = data;
+    if (!document.querySelector(".scratchtoolsFollowerCount")) {
+      if (stillComplete) {
+        var span = document.createElement("span");
+        span.textContent =
+          " (" +
+          div
+            .querySelector(".box-head > h2")
+            .textContent.split("(")[1]
+            .split(")")[0] +
+          " followers" +
+          ")";
+        span.className = "scratchtoolsFollowerCount";
+        document.querySelector(".title").appendChild(span);
+      }
     }
   }
+  ScratchTools.setDisable("follower-count", function () {
+    stillComplete = false;
+    document.querySelector(".scratchtoolsFollowerCount").remove();
+  });
+  var stillComplete = true;
+  ScratchTools.waitForElements(
+    ".title",
+    getFollowerCount,
+    "project follower count",
+    false
+  );
 }
-ScratchTools.setDisable("follower-count", function () {
-  stillComplete = false;
-  document.querySelector(".scratchtoolsFollowerCount").remove();
-});
-var stillComplete = true;
-ScratchTools.waitForElements(
-  ".title",
-  getFollowerCount,
-  "project follower count",
-  false
-);
