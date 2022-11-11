7db6984fc935549a893155acd4789227ab156b00
ElPro7777 <86174806+ElPro7777@users.noreply.github.com>
2022-11-10 23:10:38 -0300

    Create Sunky-scratch.js

---
+++ features/Sunky-scratch.js
@@ -0,0 +1,9 @@
+alert(Sunky Says: Hello)
+.rotate90 {  
+  -webkit-transform:rotate(90deg);  
+  -moz-transform: rotate(90deg);  
+  -ms-transform: rotate(90deg);  
+  -o-transform: rotate(90deg);  
+  transform: rotate(90deg);  
+}  
+alert(Sunky Says: Scratch Rotating)
