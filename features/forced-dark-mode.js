ce5dad8164cbf9c064830ea3da8a470ff50a6582
JaydenDev <willsjayden2@gmail.com>
2022-11-23 20:18:16 -0600

    feat: add forced dark mode

---
+++ features/forced-dark-mode.js
@@ -0,0 +1,19 @@
+// Forced Dark Mode
+// By JaydenDev, 2022, MIT license
+// Forces dark mode on all scratch pages (forums and editor)
+// incompatible with other dark mode features
+
+// credit to: n/a
+// https://stackoverflow.com/questions/15505225/inject-css-stylesheet-as-string-using-javascript
+function addStyle(styleString) {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+}
+
+addStyle(`
+    * {
+        background-color: invert(100%);
+        color: invert(100%);
+    }
+`)
