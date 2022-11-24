57892b432891dbe4a727c2297e270c1f5b2cb2be
jdev08 <92550746+jdev082@users.noreply.github.com>
2022-11-23 21:18:44 -0600

    feat: requested changes

--- features/forced-dark-mode.js
+++ features/forced-dark-mode.js
@@ -3,18 +3,4 @@
 // Forces dark mode on all scratch pages (forums and editor)
 // incompatible with other dark mode features
 
-// credit to: n/a
-// https://stackoverflow.com/questions/15505225/inject-css-stylesheet-as-string-using-javascript
-function addStyle(styleString) {
-    const style = document.createElement('style');
-    style.textContent = styleString;
-    document.head.append(style);
-}
-
-addStyle(`
-    * {
-        background-color: invert(100%);
-    }
-`)
-
 document.body.style.filter = 'invert(1)'
