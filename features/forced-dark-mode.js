a13c7bb3cf56ac40416c24a3b7e2681ad707a5cc
jdev08 <92550746+jdev082@users.noreply.github.com>
2022-11-23 21:29:43 -0600

    fix: reinvert images so that they aren't inverted

--- features/forced-dark-mode.js
+++ features/forced-dark-mode.js
@@ -3,4 +3,18 @@
 // Forces dark mode on all scratch pages (forums and editor)
 // incompatible with other dark mode features
 
+// credit to: n/a
+// https://stackoverflow.com/questions/15505225/inject-css-stylesheet-as-string-using-javascript
+function addStyle(styleString) {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+}
+
 document.body.style.filter = 'invert(1)'
+
+addStyle(`
+  img {
+    color: invert(100%);
+  }
+`)
