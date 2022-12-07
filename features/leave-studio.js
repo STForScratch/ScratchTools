447b4f5a35434c3ab8e05390b04761d71cdaaca8
rgantzos <info@gantzos.com>
2022-12-07 15:18:45 -0800

    leave studio button

---
+++ features/leave-studio.js
@@ -0,0 +1,66 @@
+if (
+    window.location.href
+      .toLowerCase()
+      .startsWith("https://scratch.mit.edu/studios/") &&
+    window.location.href.includes("/curators") &&
+    !document.querySelector(".scratchtoolsLeaveStudio")
+  ) {
+    const studioId = window.location.href.replace("https://", "").split("/")[2];
+  
+    async function leaveStudio() {
+      var auth = await ScratchTools.Session();
+      await fetch(
+        "https://scratch.mit.edu/site-api/users/curators-in/" +
+          studioId +
+          "/remove/?usernames=" +
+          auth.user.username,
+        {
+          headers: {
+            "x-csrftoken": ScratchTools.cookies.get("scratchcsrftoken"),
+          },
+          body: null,
+          method: "PUT",
+        }
+      );
+      window.location.href = window.location.href;
+    }
+  
+    async function getCurating() {
+      var auth = await ScratchTools.Session();
+      var response = await fetch(
+        "https://api.scratch.mit.edu/studios/" +
+          studioId +
+          "/users/" +
+          auth.user.username,
+        {
+          headers: {
+            "x-token": auth.user.token,
+          },
+        }
+      );
+      var data = await response.json();
+      if (data.curator || data.manager) {
+        ScratchTools.waitForElements(
+          ".studio-header-container.studio-managers-header",
+          function (el) {
+            var btn = document.createElement("button");
+            btn.className = "scratchtoolsLeaveStudio button";
+            btn.textContent = "Leave Studio";
+            btn.style.backgroundColor = "#ff4c4c";
+            btn.style.position = "absolute";
+            btn.style.right = "0px";
+            btn.onclick = function () {
+              if (confirm("Are you sure you want to leave the studio?")) {
+                leaveStudio();
+              }
+            };
+            el.appendChild(btn);
+          },
+          "scratchtoolsLeaveStudio",
+          false
+        );
+      }
+    }
+    getCurating();
+  }
+  
