fe70698a433f3286d9d35c6c20cc1d8a1e4e7e31
Dr-Lego <119697525+Dr-Lego@users.noreply.github.com>
2022-12-08 15:41:01 +0100

    You can now switch between the stats and "What I've been doing"

--- features/user-stats.js
+++ features/user-stats.js
@@ -6,17 +6,45 @@ async function getStats()
   if(data.statistics != undefined){
     function image(url,alt){return(`<img src='${url}' width='25' height='25' title='${alt}' style='vertical-align:middle; margin:10px'></img>`)};
     function space(){return("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")};
+    function change(){
+      if(mode == "WIBD"){
+        statistics.style.display = "block"
+        activity.style.display = "none"
+        mode = "stats"
+      }else{
+        statistics.style.display = "none"
+        activity.style.display = "block"
+        mode= "WIBD"
+      }
+    }
     function commafy(num){
       if(num == undefined){return("0")}else
       {return(parseInt(num).toLocaleString())}};
-    document.getElementsByClassName("activity-stream")[0].remove();
-    var box = document.getElementsByClassName("doing")[0];
-    var table = `${"<div style='margin:10px'>#" + commafy(data.statistics.ranks.followers) + " (#" + commafy(data.statistics.ranks.country.followers) + ")"+space()+"</div>"}${image('https://scratch.mit.edu/svgs/messages/follow.svg','Followers')}${commafy(data.statistics.followers)}${space()}<br>${image('https://scratch.mit.edu/svgs/messages/love.svg','Loves')}${commafy(data.statistics.loves)}${space()}<br>${image('https://scratch.mit.edu/svgs/messages/favorite.svg','Favorites')}${commafy(data.statistics.favorites)}${space()}<br>${image('https://scratch.mit.edu/svgs/project/views-gray.svg','Views')}${commafy(data.statistics.views)}${space()}<br>`;
-    var scratchstats = `<a href="https://scratchstats.com/${window.location.href.replaceAll("https://scratch.mit.edu/users/","")}" style="font-size:10px">View on Scratchstats</a>`
-    box.innerHTML = `<h3>Statistics</h3><div id='statistics' style='text-align:center; font-size:20px; padding:10px; background-color: var(--darkWww-box, white);border-radius: 8px;border: 1px solid var(--darkWww-border-15, #d9d9d9);'>${table}${scratchstats}</div>`.replaceAll("undefined","0")
-  	};
+    if(replaceIt == true)
+    {
+      var mode = "stats"
+      var activity = document.getElementById("activity-feed")
+      activity.style.display = "none" //remove();
+      var box = document.getElementsByClassName("doing")[0];
+      var table = `${"<div style='margin:10px'>#" + commafy(data.statistics.ranks.followers) + " (#" + commafy(data.statistics.ranks.country.followers) + ")"+space()+"</div>"}${image('https://scratch.mit.edu/svgs/messages/follow.svg','Followers')}${commafy(data.statistics.followers)}${space()}<br>${image('https://scratch.mit.edu/svgs/messages/love.svg','Loves')}${commafy(data.statistics.loves)}${space()}<br>${image('https://scratch.mit.edu/svgs/messages/favorite.svg','Favorites')}${commafy(data.statistics.favorites)}${space()}<br>${image('https://scratch.mit.edu/svgs/project/views-gray.svg','Views')}${commafy(data.statistics.views)}${space()}<br>`;
+      var scratchstats = `<a href="https://scratchstats.com/${window.location.href.replaceAll("https://scratch.mit.edu/users/","")}" style="font-size:10px">View on Scratchstats</a>`
+      var boxStyle = "background-color: var(--darkWww-box, white);border-radius: 8px;border: 1px solid var(--darkWww-border-15, #d9d9d9);padding:5px;cursor:pointer"
+      function click(type){
+        if(type){return('document.getElementById("statistics").style.display = "block"; document.getElementById("activity-feed").style.display = "none";document.getElementById("SA-WIBD").style.fontSize="80%";')}
+        else{return('document.getElementById("statistics").style.display = "none"; document.getElementById("activity-feed").style.display = "block";document.getElementById("SA-STATS").style.fontSize="80%";')}
+      }
+      var children = box.childNodes; children[1].innerHTML = `<span id='SA-STATS' onclick='${click(true)}this.style.fontSize="100%"' style='${boxStyle}'>Statistics</span> <span id='SA-WIBD' onclick='${click(false)}this.style.fontSize = "100%"' style='${boxStyle};font-size:80%'>What I've been doing</span>`;//children[1].onclick = change
+      var divText = `<div id='statistics' style='text-align:center; font-size:20px; padding:10px; background-color: var(--darkWww-box, white);border-radius: 8px;border: 1px solid var(--darkWww-border-15, #d9d9d9);'>${table}${scratchstats}</div>`.replaceAll("undefined","0")
+      var div = document.createElement("div");
+      div.innerHTML = divText;
+      box.appendChild(div);
+      var statistics = document.getElementById("statistics")
+      //box.innerHTML = `<h3 onclick='alert("Test")'>Statistics</h3>${box.innerHTML}<div id='statistics' style='text-align:center; font-size:20px; padding:10px; background-color: var(--darkWww-box, white);border-radius: 8px;border: 1px solid var(--darkWww-border-15, #d9d9d9);'>${table}${scratchstats}</div>`.replaceAll("undefined","0")
+    }
+    };
   }
 }
 
+var replaceIt = true //Just for testing. 
 ScratchTools.waitForElements("#activity-feed", getStats, "getUserStatistics", false)
 
