dd8d226c1b7d1b1a9137eb4f66168c58bd9e18c0
rgantzos <info@gantzos.com>
2023-01-16 08:06:40 -0800

    scroll project titles

---
+++ features/scroll-project-titles.js
@@ -0,0 +1,12 @@
+ScratchTools.styles.add(`
+.project-title.no-edit {
+    overflow-x: scroll !important;
+    text-overflow: clip !important;
+}`, "scroll project titles")
+
+ScratchTools.setDisable("scroll-project-titles", function() {
+    if (document.querySelector(".project-title.no-edit")) {
+        document.querySelector(".project-title.no-edit").scrollLeft = 0
+    }
+    ScratchTools.styles.removeStyleById("scroll project titles")
+})
