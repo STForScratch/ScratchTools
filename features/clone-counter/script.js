86962fc53203a71d3b7c9700241fdaca576346b2
rgantzos <info@gantzos.com>
2023-04-21 09:50:56 -0700

    Better clone counter

---
+++ features/clone-counter/script.js
@@ -0,0 +1,64 @@
+if (!document.querySelector(".scratchtools-main-clone-counter")) {
+  ScratchTools.waitForElements(
+    "div[class^='stage-header_stage-menu-wrapper']",
+    function () {
+      var div = document.createElement("div");
+      div.className = "scratchtools-main-clone-counter";
+      var img = document.createElement("img");
+      img.src = ScratchTools.Resources["clones"];
+      var span = document.createElement("span");
+      span.textContent = "0 clones";
+      div.appendChild(img);
+      div.appendChild(span);
+      ScratchTools.appendToSharedSpace({
+        space: "afterStopButton",
+        element: div,
+        order: 0,
+      });
+    },
+    "clone-counter-above-stage",
+    false
+  );
+  var interval = setInterval(function () {
+    if (document.querySelector(".scratchtools-main-clone-counter")) {
+      var counter = document.querySelector(
+        ".scratchtools-main-clone-counter span"
+      );
+      var count = (
+        ScratchTools.Scratch?.vm?.runtime?._cloneCounter || 0
+      ).toString();
+      counter.textContent = count + " clones";
+    } else if (
+      document.querySelector("div[class^='stage-header_stage-menu-wrapper']")
+    ) {
+      var div = document.createElement("div");
+      div.className = "scratchtools-main-clone-counter";
+      var img = document.createElement("img");
+      img.src = ScratchTools.Resources["clones"];
+      var span = document.createElement("span");
+      span.textContent = "0 clones";
+      div.appendChild(img);
+      div.appendChild(span);
+      ScratchTools.appendToSharedSpace({
+        space: "stageHeader",
+        element: div,
+        order: 0,
+      });
+    }
+  }, 100);
+}
+
+document
+  .querySelector("div[class^='stage-header_stage-size-toggle-group_']")
+  .addEventListener("click", function (event) {
+    var counter = document.querySelector(
+      ".scratchtools-main-clone-counter span"
+    );
+    if (event.target.closest("[class*='stage-header_stage-button-first']")) {
+      counter.style.display = "none";
+    } else if (
+      event.target.closest("[class*='stage-header_stage-button-last']")
+    ) {
+      counter.style.display = null;
+    }
+  });
