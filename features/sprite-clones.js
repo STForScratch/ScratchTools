858d59749f131fa8b2e5f8aa8c7470755e5b1d27
Wilamaxin <63302372+Wilamaxin@users.noreply.github.com>
2023-04-23 20:13:31 +0200

    Better compatibility with "Compact Editor"

--- features/sprite-clones.js
+++ features/sprite-clones.js
@@ -9,21 +9,21 @@ if (
       document.querySelector(
         "span.scratchtoolsSpriteCloneCounter"
       ).textContent = `${(
-        ScratchTools.Scratch.vm.editingTarget.sprite.clones.length - 1
+        ScratchTools.Scratch.vm.editingTarget?.sprite.clones.length - 1
       ).toString()} clones`;
     } else {
       var foundIt = false;
       document.querySelectorAll("div").forEach(function (el) {
         if (foundIt === false) {
-          if (el.className.startsWith("sprite-info_row_")) {
+          if (el.className.startsWith("sprite-info_row_1om5V") &&! el.className.includes("sprite-info_row-primary_10JrS")) {
             foundIt = true;
             var div = document.createElement("div");
             div.className = "sprite-info_group_14-B_";
-            div.innerHTML = `<label class="label_input-group_2vTky"><span style="font-size: 0.625rem; font-weight: bold;" class="scratchtoolsSpriteCloneCounter">${(
-              ScratchTools.Scratch.vm.editingTarget.sprite.clones.length - 1
+            div.innerHTML = `<label class="label_input-group_2vTky"><span style="font-size: 0.625rem; font-weight: bold; margin-left:.2rem" class="scratchtoolsSpriteCloneCounter">${(
+              ScratchTools.Scratch.vm.editingTarget?.sprite.clones.length - 1
             ).toString()} clones</span></label>`;
             el.appendChild(div);
-            el.firstChild.lastChild.style.width = "7rem";
+			            el.lastChild.lastChild.style.width = "3rem";
           }
         }
       });
