2c9e4565b97dfabc10abb3ec10af55ad6c8ad2cb
rgantzos <info@gantzos.com>
2023-05-11 14:15:40 -0700

    Better changelog

--- changelog/script.js
+++ changelog/script.js
@@ -3,42 +3,58 @@ document.querySelector("title").textContent = "What's New in v" + version;
 document.querySelector("h1").textContent = "What's New in v" + version;
 
 async function getChanges() {
-    const changes = await (await fetch("./changes.json")).json()
-    if (changes.new.length) {
-        var h3 = document.createElement("h3")
-        h3.textContent = "New Features"
-        var ul = document.createElement("ul")
-        changes.new.forEach(function(el) {
-            var li = document.createElement("li")
-            li.textContent = el
-            ul.appendChild(li)
-        })
-        document.querySelector(".right").appendChild(h3)
-        document.querySelector(".right").appendChild(ul)
+  var newFeatures = await (await fetch("/features/features.json")).json();
+  for (var i in newFeatures) {
+    var feature = newFeatures[i];
+    if (feature.versionAdded === "v" + version) {
+      var div = document.createElement("div");
+      var h2 = document.createElement("h2");
+      if (feature.version === 2) {
+        var data = await (
+          await fetch(`/features/${feature.id}/data.json`)
+        ).json();
+        h2.textContent = data.title;
+      } else {
+        h2.textContent = data.title;
+      }
+      div.appendChild(h2);
+      var p = document.createElement("p");
+      p.textContent = data?.description || feature.description;
+      div.appendChild(p);
+      var design = document.createElement("div");
+      design.className = "colorful-design";
+      div.prepend(design);
+      if (i > 2) {
+        div.className = "wide-feature feature";
+        document.querySelector(".new-features-full").prepend(div);
+      } else {
+        div.className = "feature";
+        document.querySelector(".new-features").prepend(div);
+      }
     }
-    if (changes.enhanced.length) {
-        var h3 = document.createElement("h3")
-        h3.textContent = "Enhancements"
-        var ul = document.createElement("ul")
-        changes.enhanced.forEach(function(el) {
-            var li = document.createElement("li")
-            li.textContent = el
-            ul.appendChild(li)
-        })
-        document.querySelector(".right").appendChild(h3)
-        document.querySelector(".right").appendChild(ul)
-    }
-    if (changes.fixed.length) {
-        var h3 = document.createElement("h3")
-        h3.textContent = "Bug Fixes"
-        var ul = document.createElement("ul")
-        changes.fixed.forEach(function(el) {
-            var li = document.createElement("li")
-            li.textContent = el
-            ul.appendChild(li)
-        })
-        document.querySelector(".right").appendChild(h3)
-        document.querySelector(".right").appendChild(ul)
-    }
+  }
+  const changes = await (await fetch("./changes.json")).json();
+  if (changes.enhanced.length) {
+    var h3 = document.createElement("h2");
+    h3.textContent = "Enhancements";
+    document.querySelector(".better-bugs .left").appendChild(h3);
+    changes.enhanced.forEach(function (el) {
+      var div = document.createElement("div");
+      div.textContent = el;
+      div.className = "better-item";
+      document.querySelector(".better-bugs .left").appendChild(div);
+    });
+  }
+  if (changes.fixed.length) {
+    var h3 = document.createElement("h2");
+    h3.textContent = "Bug Fixes";
+    document.querySelector(".better-bugs .right").appendChild(h3);
+    changes.fixed.forEach(function (el) {
+      var div = document.createElement("div");
+      div.textContent = el;
+      div.className = "bug-item";
+      document.querySelector(".better-bugs .right").appendChild(div);
+    });
+  }
 }
-getChanges()
+getChanges();
