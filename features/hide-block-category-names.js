94fc522aae80747b97d2f0d679569ddab79fc9b2
rgantzos <info@gantzos.com>
2023-05-18 07:02:10 -0700

    Revert "More options for `hide-block-category-names`"
    This reverts commit 1b8ddea2e832598fc871f4da9c4e442b51a09182.

---
+++ features/hide-block-category-names.js
@@ -0,0 +1,15 @@
+var style = document.createElement("style");
+style.textContent = `
+.scratchCategoryItemBubble {
+    border-radius: .2rem !important;
+    width: 2rem !important;
+}
+
+.scratchCategoryMenuItemLabel {
+    display: none !important;
+}
+`;
+document.body.appendChild(style);
+ScratchTools.setDisable("hide-block-category-names", function () {
+  style.remove();
+});
