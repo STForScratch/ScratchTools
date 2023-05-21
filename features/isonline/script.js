5f8516ccccae739cded1b1fee737f5265e1dafe6
rgantzos <info@gantzos.com>
2023-05-20 20:30:52 -0700

    Online detection

---
+++ features/isonline/script.js
@@ -0,0 +1,17 @@
+async function checkUser() {
+    var data = await (await fetch(`https://data.scratchtools.app/isonline/${window.location.pathname.split("/")[2]}/`)).json()
+    if (data.scratchtools) {
+        var span = document.createElement("span")
+        span.className = "ste-isonline "+(data.online ? "ste-detect-online" : "ste-detect-offline")
+        span.textContent = data.online ? "Online" : "Offline"
+        span.title = "This was added by ScratchTools."
+        document.querySelector(".location").appendChild(span)
+    } else {
+        var span = document.createElement("span")
+        span.className = "ste-isonline unsure"
+        span.textContent = "(Doesn't use ScratchTools online detection)"
+        span.title = "This was added by ScratchTools."
+        document.querySelector(".location").appendChild(span)
+    }
+}
+checkUser()
