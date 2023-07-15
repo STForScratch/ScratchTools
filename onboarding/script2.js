0b2f902cccd853c9e91faaefab158c588da4bbdd
Daniel Barton <llama552621@gmail.com>
2023-07-15 12:51:11 +1000

    Update script2.js

--- onboarding/script2.js
+++ onboarding/script2.js
@@ -5,7 +5,7 @@ const section = {
     1: gEBI("section_1"),
     2: gEBI("section_2"),
     3: gEBI("section_3"),
-    4: gEBI("sectio_4")
+    4: gEBI("section_4")
 }
 
 async function start(){
@@ -13,12 +13,8 @@ async function start(){
     section[1].classList.add("hidden");
     section[2].classList.remove("hidden");
 
-    features = document.querySelector("div[data-function=features]")
-
-    var featuredFeatures = await (await fetch("./featured.json")).json();
-  featuredFeatures.forEach(async function (ftr) {
-    features.appendChild(await createFeature(ftr));
-  });
+    
+    
 }
 
 document.querySelector("button[data-function=start]").onclick = start;
