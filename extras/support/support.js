99cdc744cd00f4293a6afde6a30af212cd5af9e8
rgantzos <info@gantzos.com>
2023-07-20 15:35:42 -0700

    Fix a few random bugs

--- extras/support/support.js
+++ extras/support/support.js
@@ -9,7 +9,7 @@ async function start() {
 }
 start();
 
-document.querySelector(".terms button").addEventListener("click", agree)
+document.querySelector(".terms button").addEventListener("click", agree);
 
 async function agree() {
   document.querySelector(".terms").style.display = "none";
@@ -17,25 +17,29 @@ async function agree() {
   start();
 }
 
-document.querySelector(".topics button").addEventListener("click", function() {
-    document.querySelector(".normal").style.display = "none";
-    document.querySelector(".additional-support").style.display = null;
-})
+document.querySelector(".topics button").addEventListener("click", function () {
+  document.querySelector(".normal").style.display = "none";
+  document.querySelector(".additional-support").style.display = null;
+});
 
-document.querySelector(".option[data-option=discord]").addEventListener("click", function() {
+document
+  .querySelector(".option[data-option=discord]")
+  .addEventListener("click", function () {
     chrome.tabs.create({
-        url: "https://discord.gg/TcgexZDypB"
-    })
-})
+      url: "https://discord.gg/rwAs5jDrTQ",
+    });
+  });
 
-document.querySelector(".option[data-option=twitter]").addEventListener("click", function() {
+document
+  .querySelector(".option[data-option=twitter]")
+  .addEventListener("click", function () {
     chrome.tabs.create({
-        url: "https://twitter.com/intent/tweet?text=@STForScratch%20I%20need%20help."
-    })
-})
+      url: "https://twitter.com/intent/tweet?text=@STForScratch%20I%20need%20help.",
+    });
+  });
 
-document.querySelector(".option[data-option=chat]").onclick = function() {
-    chrome.tabs.create({
-        url: "https://scratch.mit.edu/scratchtools/support/auth/"
-    })
-}
+document.querySelector(".option[data-option=chat]").onclick = function () {
+  chrome.tabs.create({
+    url: "https://scratch.mit.edu/scratchtools/support/auth/",
+  });
+};
