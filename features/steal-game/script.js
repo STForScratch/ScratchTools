7902ae01bd64df95a72f8af1a4db4512376c76f3
callum <116295240+callumjt@users.noreply.github.com>
2023-08-07 07:24:29 +0100

    Update script.js

--- features/steal-game/script.js
+++ features/steal-game/script.js
@@ -1,16 +1,11 @@
-var enabledStealGame = true;
-
 if (window.location.href.startsWith("https://scratch.mit.edu/projects/")) {
-    if (enabledStealGame) {
-        var remixButton = document.querySelector('.remix-button')
-        var remixText = remixButton.querySelector('span');
-        remixText.textContent = "Steal game"
-    }
+    var remixButton = document.querySelector('.remix-button')
+    var remixText = remixButton.querySelector('span');
+    remixText.textContent = "Steal game"
 }
 
 ScratchTools.setDisable("steal-game", function () {
-    if (window.location.href.startsWith("https://scratch.mit.edu/projects/")) {
-        enabledStealGame = false;
+    if (window.location.href.startsWith("https://scratch.mit.edu/projects) {
         var remixButton = document.querySelector('.remix-button')
         var remixText = remixButton.querySelector('span');
         remixText.textContent = "Remix"
