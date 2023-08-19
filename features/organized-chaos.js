e6658d3097c11832ccbeb35e1759d1ed317dd2ec
rgantzos <86856959+rgantzos@users.noreply.github.com>
2023-08-18 17:14:38 -0700

    Revert "Organized chaos"

--- features/organized-chaos.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {
-  javascript:(function(){['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){document.body.style[prefix + 'transform'] = 'rotate(180deg)';});}());
-  document.body.contentEditable = 'true';
-  document.designMode = 'on';
-  void 0;
-}, 4000);
