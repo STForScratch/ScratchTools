e077c05c028d1985a5837960520af83a990cbdb1
Daniel4-Scratch <daniel4scratchhelp@gmail.com>
2023-12-24 13:46:01 +1100

    Make the base of the clicker game

--- extras/game/script.js
+++ extras/game/script.js
@@ -1,16 +1,43 @@
-// finally got it to work :\
-
 const run = async () => {
+    try {
+        let { game: storage } = await chrome.storage.sync.get("game");
+        var starterStorage = { "version": 1, "clicks": 0 };
 
-    let { game: storage } = await chrome.storage.sync.get("game");
-    var starterStorage = {"version":1, "clicks":0}
-
-    // check if storage is trust worthy
-    if (storage) {
-        console.log(storage)
-    }else{
-        await chrome.storage.sync.set({"game":JSON.stringify(starterStorage)});
+        // check if storage is trustworthy
+        if (storage && typeof storage === 'object' && typeof storage.clicks === 'number') {
+            document.querySelector('.text-layer h1').textContent = String(storage.clicks);
+            console.log(storage.clicks);
+            document.getElementById("click").draggable = false;
+            document.getElementById("click").addEventListener("click", async () => {
+                storage.clicks += 1;
+                document.querySelector('.text-layer h1').textContent = String(storage.clicks);
+                await chrome.storage.sync.set({ "game": storage });
+            });
+        } else {
+            await chrome.storage.sync.set({ "game": starterStorage });
+            location.reload();
+        }
+    } catch (error) {
+        console.error("Error during storage retrieval:", error);
     }
 }
+
+const reset = async () => {
+    try {
+        await chrome.storage.sync.remove("game");
+        location.reload();
+    } catch (error) {
+        console.error("Error during storage removal:", error);
+    }
+}
+// JavaScript
+var clickElement = document.getElementById('click');
+
+clickElement.addEventListener('click', function() {
+    // Remove the pop class and force a reflow to allow the animation to be restarted mid-animation
+    this.classList.remove('pop');
+    void this.offsetWidth;
+    this.classList.add('pop');
+});
 
 run();
