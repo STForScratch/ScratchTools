09ae7fb44b67a0e567165b9d9ca97123933b1c55
Masaabu <Masa2525ym@gmail.com>
2024-02-14 06:24:32 +0900

    project-words-filter

---
+++ features/project-words-filter/script.js
@@ -0,0 +1,38 @@
+export default async function ({ feature, console }) {
+    let filterWords = feature.settings.get("filter-words");
+    let blur = feature.settings.get("blur");
+    let WordList = filterWords.split(',').filter(item => item !== "");;
+
+    ScratchTools.waitForElements("div.thumbnail.project", filter)
+    feature.settings.addEventListener("changed", function({ key, value }) {
+        switch (key) {
+            case "filter-words":
+                filterWords = value.toString();
+                WordList = filterWords.split(',').filter(item => item !== "");;
+                break;
+            case "blur":
+                blur = value
+                break;
+            default:
+                break;
+        }
+        document.querySelectorAll('.thumbnail.project').forEach(filter)
+
+    });
+
+    async function filter(element) {
+        let title = element.querySelector('.thumbnail-title a').getAttribute('title');
+        if (WordList.some(item => title.includes(item))) {
+            if (blur) {
+                element.classList.add('filter-blur')
+                element.classList.remove('filter-hide')
+            } else {
+                element.classList.add('filter-hide')
+                element.classList.remove('filter-blur')
+            }
+        } else {
+            element.classList.remove('filter-blur')
+            element.classList.remove('filter-hide')
+        }
+    };
+}
