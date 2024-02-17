d922db6362d25049b26cee4b233ffa20d9b3568d
MaterArc <105017592+MaterArc@users.noreply.github.com>
2024-02-17 13:41:29 -0500

    Support lowercase filter words
    Remove the case-sensitive aspect of removing or blurring filter words.

--- features/project-words-filter/script.js
+++ features/project-words-filter/script.js
@@ -1,38 +1,38 @@
 export default async function ({ feature, console }) {
     let filterWords = feature.settings.get("filter-words");
     let blur = feature.settings.get("blur");
-    let WordList = filterWords.split(',').filter(item => item !== "");;
+    let WordList = filterWords.split(',').filter(item => item !== "");
 
-    ScratchTools.waitForElements("div.thumbnail.project", filter)
+    ScratchTools.waitForElements("div.thumbnail.project", filter);
+
     feature.settings.addEventListener("changed", function({ key, value }) {
         switch (key) {
             case "filter-words":
                 filterWords = value.toString();
-                WordList = filterWords.split(',').filter(item => item !== "");;
+                WordList = filterWords.split(',').filter(item => item !== "");
                 break;
             case "blur":
-                blur = value
+                blur = value;
                 break;
             default:
                 break;
         }
-        document.querySelectorAll('.thumbnail.project').forEach(filter)
-
+        document.querySelectorAll('.thumbnail.project').forEach(filter);
     });
 
     async function filter(element) {
-        let title = element.querySelector('.thumbnail-title a').getAttribute('title');
-        if (WordList.some(item => title.includes(item))) {
+        let title = element.querySelector('.thumbnail-title a').getAttribute('title').toLowerCase();
+        if (WordList.some(item => title.includes(item.toLowerCase()))) {
             if (blur) {
-                element.classList.add('filter-blur')
-                element.classList.remove('filter-hide')
+                element.classList.add('filter-blur');
+                element.classList.remove('filter-hide');
             } else {
-                element.classList.add('filter-hide')
-                element.classList.remove('filter-blur')
+                element.classList.add('filter-hide');
+                element.classList.remove('filter-blur');
             }
         } else {
-            element.classList.remove('filter-blur')
-            element.classList.remove('filter-hide')
+            element.classList.remove('filter-blur');
+            element.classList.remove('filter-hide');
         }
     };
 }
