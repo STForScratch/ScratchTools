d41edc1016ec928c8ff76295a330bfb48018c525
blazgocompany <96586789+blazgocompany@users.noreply.github.com>
2024-06-28 08:01:26 -0700

    Add Scratch Profile styles

---
+++ features/new-look/scratch-profile.js
@@ -0,0 +1,18 @@
+export default async function ({ feature }) {
+    let player = await ScratchTools.waitForElement(".user-content div.player");
+    let pdata = await ScratchTools.waitForElement(".profile-data .box-head");
+    pdata.appendChild(player)
+    player.remove()
+    if (feature.self.enabled) {
+      docume.querySelector("h4").textContent = "Dumpster Fire";
+    }
+  
+    feature.addEventListener("disabled", function () {
+      box.querySelector("h4").textContent = oldContent;
+    });
+  
+    feature.addEventListener("enabled", function () {
+      box.querySelector("h4").textContent = "Dumpster Fire";
+    });
+  }
+  
