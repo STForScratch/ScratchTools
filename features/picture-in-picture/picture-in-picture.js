48191f29d3779e3d938525fda10f34f1ee05292a
stio <dev@stio.studio>
2024-08-01 18:39:41 +0200

    Using the Document Picture-in-Picture API (optional)

--- features/picture-in-picture/picture-in-picture.js
+++ features/picture-in-picture/picture-in-picture.js
@@ -1,14 +1,4 @@
 export default async function ({ feature, console }) {
-	const canvas = feature.traps.vm.renderer.canvas;
-
-	let video = document.createElement("video");
-	// video.setAttribute("controls", "controls");
-	video.setAttribute("autoplay", "autoplay");
-	video.setAttribute("style", "width: 100%; height: 100%");
-	// document.querySelector(".preview .inner").append(video);
-
-	video.srcObject = canvas.captureStream(30)
-
 	await new Promise(async (resolve, reject) => {
 		(async () => {
 			const rem = await ScratchTools.waitForElement(".preview .inner .flex-row.action-buttons")
@@ -20,6 +10,7 @@ export default async function ({ feature, console }) {
 		})();
 	})
 
+	const canvas = feature.traps.vm.renderer.canvas;
 	let openPopup = document.createElement("button");
 
 	ScratchTools.waitForElements(".preview .inner .flex-row.action-buttons", async function (row) {
@@ -45,12 +36,110 @@ export default async function ({ feature, console }) {
 		})
 	})
 
-	function popup() {
-		try {
-			video.requestPictureInPicture()
+	let popup;
+
+	if (feature.settings.get("interactivity-PiP")) {
+		if (!"documentPictureInPicture" in window) console.error("Picture in Picture not supported")
+
+		let pipWindow
+
+		let docPopup = document.createElement("div");
+		docPopup.insertAdjacentHTML("afterbegin", await (await fetch(feature.self.getResource("popup-html"))).text())
+		docPopup = docPopup.querySelector("div.popup-GUI")
+
+		let video = docPopup.querySelector("video");
+
+		const greenFlag = document.querySelector(".green-flag_green-flag_1kiAo")
+		docPopup.querySelector(".popup-greenflag").addEventListener("click", () => {
+			greenFlag.click()
+		});
+		const redFlag = document.querySelector(".stop-all_stop-all_1Y8P9")
+		docPopup.querySelector(".popup-redflag").addEventListener("click", () => {
+			redFlag.click()
+		});
+
+		// video.addEventListener("mousedown", (old_event) => {
+		function translateEvent_pointer(old_event) {
+			// Calculate the canvas position relative to the viewport
+			const a_rect = canvas.getBoundingClientRect();
+			const b_rect = video.getBoundingClientRect();
+
+			// console.log(old_event)
+			// Create a new event with the adjusted coordinates
+
+			let new_event = new old_event.constructor(old_event.type, {
+				bubbles: old_event.bubbles,
+				cancelable: old_event.cancelable,
+				clientX: (old_event.clientX - b_rect.left) * (a_rect.width / b_rect.width) + a_rect.left,
+				clientY: (old_event.clientY - b_rect.top) * (a_rect.height / b_rect.height) + a_rect.top,
+				// Copy over other necessary properties from the old event
+				screenX: (old_event.screenX - pipWindow.screenLeft + window.screenLeft - b_rect.left) * (a_rect.width / b_rect.width) + a_rect.left,
+				screenY: (old_event.screenY - pipWindow.screenTop + window.screenTop - b_rect.top) * (a_rect.height / b_rect.height) + a_rect.top,
+				layerX: old_event.layerX,
+				layerY: old_event.layerY,
+				button: old_event.button,
+				buttons: old_event.buttons,
+				relatedTarget: old_event.relatedTarget,
+				altKey: old_event.altKey,
+				ctrlKey: old_event.ctrlKey,
+				shiftKey: old_event.shiftKey,
+				metaKey: old_event.metaKey,
+				movementX: old_event.movementX,
+				movementY: old_event.movementY,
+			});
+
+			// Dispatch the new event
+			canvas.dispatchEvent(new_event);
 		}
-		catch {
-			console.log("Picture in Picture not supported or failed to request")
+		video.addEventListener("mousedown", translateEvent_pointer)
+		video.addEventListener("mouseup", translateEvent_pointer)
+		video.addEventListener("mousemove", translateEvent_pointer)
+		video.addEventListener("wheel", translateEvent_pointer)
+		video.addEventListener("touchstart", translateEvent_pointer)
+		video.addEventListener("touchend", translateEvent_pointer)
+		video.addEventListener("touchmove", translateEvent_pointer)
+
+		function translateEvent_key(old_event) {
+			let new_event = new KeyboardEvent(old_event.type, old_event)
+			document.dispatchEvent(new_event);
+		}
+
+		let buttonClickedTimes = 0
+		popup = async function () {
+			if (buttonClickedTimes === 0) {
+				video.srcObject = canvas.captureStream()
+				buttonClickedTimes++
+			}
+			// Open a Picture-in-Picture window.
+			pipWindow = await window.documentPictureInPicture.requestWindow({
+				width: canvas.width,
+				height: canvas.height + 20 + 6 * 2,
+			});
+
+			// Move the player to the Picture-in-Picture window.
+			pipWindow.document.body.append(docPopup);
+
+			pipWindow.document.addEventListener("keydown", translateEvent_key)
+			pipWindow.document.addEventListener("keypress", translateEvent_key)
+			pipWindow.document.addEventListener("keyup", translateEvent_key)
+		}
+	}
+	else {
+		let video = document.createElement("video");
+		// video.setAttribute("controls", "controls");
+		video.setAttribute("autoplay", "autoplay");
+		video.setAttribute("style", "width: 100%; height: 100%");
+		// document.querySelector(".preview .inner").append(video);
+
+		video.srcObject = canvas.captureStream()
+
+		popup = function () {
+			try {
+				video.requestPictureInPicture()
+			}
+			catch {
+				console.log("Picture in Picture not supported or failed to request")
+			}
 		}
 	}
 }
