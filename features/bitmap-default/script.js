ea07bd637910e00c14563775d58749a06c334fad
MaterArc <105017592+MaterArc@users.noreply.github.com>
2024-12-30 10:41:57 -0500

    Revert

--- features/bitmap-default/script.js
+++ features/bitmap-default/script.js
@@ -2,9 +2,8 @@ export default async function ({ feature }) {
   ScratchTools.waitForElements("body", () => {
     const observer = new MutationObserver(() => {
       const bitmapButton = document.querySelector(".paint-editor_bitmap-button_OEHDO");
-      if (bitmapButton && feature.self.enabled && !bitmapButton.classList.contains("clicked")) {
+      if (bitmapButton && feature.self.enabled) {
         bitmapButton.click();
-        bitmapButton.classList.add("clicked");
       }
     });
     observer.observe(document.body, { childList: true, subtree: true });
