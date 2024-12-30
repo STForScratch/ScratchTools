7712806131ec8e0d0ed35c57c5918242f319e982
MaterArc <105017592+MaterArc@users.noreply.github.com>
2024-12-29 21:49:41 -0500

    Better method

--- features/bitmap-default/script.js
+++ features/bitmap-default/script.js
@@ -1,8 +1,11 @@
-export default async function ({ feature, console }) {
-    ScratchTools.waitForElements(".paint-editor_bitmap-button_OEHDO", (bitmapButton) => {
-      if (feature.self.enabled) {
+export default async function ({ feature }) {
+  ScratchTools.waitForElements("body", () => {
+    const observer = new MutationObserver(() => {
+      const bitmapButton = document.querySelector(".paint-editor_bitmap-button_OEHDO");
+      if (bitmapButton && feature.self.enabled) {
         bitmapButton.click();
       }
     });
-  }
-  
+    observer.observe(document.body, { childList: true, subtree: true });
+  });
+}
