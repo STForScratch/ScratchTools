729b056777385b19fec1d4d8b024259eb31c8cd3
MaterArc <105017592+MaterArc@users.noreply.github.com>
2024-12-30 10:40:29 -0500

    Dynamic

--- features/bitmap-default/script.js
+++ features/bitmap-default/script.js
@@ -2,8 +2,9 @@ export default async function ({ feature }) {
   ScratchTools.waitForElements("body", () => {
     const observer = new MutationObserver(() => {
       const bitmapButton = document.querySelector(".paint-editor_bitmap-button_OEHDO");
-      if (bitmapButton && feature.self.enabled) {
+      if (bitmapButton && feature.self.enabled && !bitmapButton.classList.contains("clicked")) {
         bitmapButton.click();
+        bitmapButton.classList.add("clicked");
       }
     });
     observer.observe(document.body, { childList: true, subtree: true });
