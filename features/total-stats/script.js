11a2715ea0336ffd8f9cfc98af1a20f0e8e1e41d
MaterArc <105017592+MaterArc@users.noreply.github.com>
2025-07-02 17:39:08 -0400

    Fix styling

--- features/total-stats/script.js
+++ features/total-stats/script.js
@@ -2,72 +2,71 @@ export default async function ({ feature }) {
   await ScratchTools.waitForElement(".box-head");
 
   const username = window.location.pathname.split("/")[2];
-  const res = await fetch(
-    `https://scratchdata.vercel.app/api/user-stats/${username}`
-  );
+  const res = await fetch(`https://scratchdata.vercel.app/api/user-stats/${username}`);
   if (!res.ok) return;
 
   const data = await res.json();
   const format = (n) => n.toLocaleString();
 
-  const box = document.createElement("div");
-  box.className = "ste-box ste-prevent-select";
-
-  const boxHead = document.createElement("div");
-  boxHead.className = "ste-box-head";
-
-  const heading = document.createElement("h4");
-  heading.textContent = "Total Project Stats";
-
-  boxHead.appendChild(heading);
-  box.appendChild(boxHead);
-
-  const boxContent = document.createElement("div");
-  boxContent.className = "ste-box-content ste-tps-content";
-
   const stats = [
     {
-      src: "https://scratch.mit.edu/svgs/project/love-red.svg",
+      icon: "https://scratch.mit.edu/svgs/project/love-red.svg",
       alt: "Loves",
       value: data.totalLoves,
     },
     {
-      src: "https://scratch.mit.edu/svgs/project/fav-yellow.svg",
+      icon: "https://scratch.mit.edu/svgs/project/fav-yellow.svg",
       alt: "Favorites",
       value: data.totalFavorites,
     },
     {
-      src: "https://scratch.mit.edu/svgs/project/views-gray.svg",
+      icon: "https://scratch.mit.edu/svgs/project/views-gray.svg",
       alt: "Views",
       value: data.totalViews,
     },
     {
-      src: "https://raw.githubusercontent.com/scratchfoundation/scratch-www/a9cf52cd7fbec834897587dd9e17d648ba0a38b2/static/svgs/messages/remix.svg",
+      icon: "https://raw.githubusercontent.com/scratchfoundation/scratch-www/a9cf52cd7fbec834897587dd9e17d648ba0a38b2/static/svgs/messages/remix.svg",
       alt: "Remixes",
       value: data.totalRemixes,
     },
   ];
 
+  const box = document.createElement("div");
+  box.className = "box slider-carousel-container prevent-select ste-tps-box";
+  box.dataset.stFeature = "total-project-stats";
+
+  const head = document.createElement("div");
+  head.className = "box-head";
+
+  const title = document.createElement("h4");
+  title.textContent = "Total Project Stats";
+  head.appendChild(title);
+  box.appendChild(head);
+
+  const content = document.createElement("div");
+  content.className = "box-content slider-carousel horizontal ste-tps-content";
+
   for (const stat of stats) {
     const statDiv = document.createElement("div");
     statDiv.className = "ste-tps-stat";
 
-    const img = document.createElement("img");
-    img.src = stat.src;
-    img.width = 32;
-    img.height = 32;
-    img.alt = stat.alt;
+    const icon = document.createElement("img");
+    icon.src = stat.icon;
+    icon.width = 32;
+    icon.height = 32;
+    icon.alt = stat.alt;
+    icon.style.display = "block";
 
-    const valueDiv = document.createElement("div");
-    valueDiv.textContent = format(stat.value);
-    valueDiv.className = "ste-tps-value";
+    const value = document.createElement("div");
+    value.className = "ste-tps-value";
+    value.textContent = format(stat.value);
 
-    statDiv.appendChild(img);
-    statDiv.appendChild(valueDiv);
-    boxContent.appendChild(statDiv);
+    statDiv.appendChild(icon);
+    statDiv.appendChild(value);
+    content.appendChild(statDiv);
   }
 
-  box.appendChild(boxContent);
+  box.appendChild(content);
 
   const boxHeads = document.querySelectorAll(".box-head");
   if (boxHeads.length >= 5) {
